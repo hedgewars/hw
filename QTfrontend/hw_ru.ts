2236f0352b84cd16b69297c016ab066e74b6294f
unc0rr <unc0rr@gmail.com>
2005-10-26 17:26:39 +0000

    Start to write QT frontend
    --HG--
    extra : convert_revision : svn%3A99beacd1-defe-0310-bff8-d3e2e4bee508/trunk%4021

---
+++ QTfrontend/hw_ru.ts
@@ -0,0 +1,53 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>HWForm</name>
+    <message>
+        <source>Hedgewars</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>???°??????????????</translation>
+    </message>
+    <message>
+        <source>Local game</source>
+        <translation>???????°?»?????°?? ???????°</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>??????????</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>???°?·?°??</translation>
+    </message>
+    <message>
+        <source>640x480</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>800x600</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>1024x768</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>1280x1024</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Fatal error</source>
+        <translation>?????????????µ?????°?? ???????±???°</translation>
+    </message>
+    <message>
+        <source>Could not start engine.</source>
+        <translation>???µ ???????? ?·?°?????????????? ???????¶????.</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>??????????</translation>
+    </message>
+</context>
+</TS>
---
+++ QTfrontend/hwconsts.h
@@ -0,0 +1,38 @@
+/*
+ * Hedgewars, a worms-like game
+ * Copyright (c) 2005 Andrey Korotaev <unC0Rr@gmail.com>
+ *
+ * Distributed under the terms of the BSD-modified licence:
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * with the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+const char resolutions[2][4][5] = 
+{
+	{"640", "800", "1024", "1280"},
+	{"480", "600",  "768", "1024"}
+};
