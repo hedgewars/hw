e666f043348135a40b4f0f4a44f5dea8fcf8217b
displacer <disinbox@gmail.com>
2006-09-03 14:34:08 +0000

    margin reduced (space between commands in teamselect widget)
    --HG--
    extra : convert_revision : svn%3A99beacd1-defe-0310-bff8-d3e2e4bee508/trunk%40138

--- QTfrontend/teamselhelper.cpp
+++ QTfrontend/teamselhelper.cpp
@@ -16,19 +16,22 @@ TeamShowWidget::TeamShowWidget(HWTeam team, bool isPlaying, QWidget * parent) :
   QWidget(parent), mainLayout(this), m_team(team), m_isPlaying(isPlaying)
 {
   mainLayout.setSpacing(1);
-  this->setMaximumHeight(45);
+  mainLayout.setMargin(2);
+  this->setMaximumHeight(35);
   QPixmap* px=new QPixmap(QPixmap(datadir->absolutePath() + "/Forts/" + m_team.Fort + "L.png").scaled(40, 40));
+
+  QPalette newPalette = palette();
+  newPalette.setColor(QPalette::Button, palette().color(backgroundRole()));
 
   QPushButton* butt=new QPushButton(*px, "", this);
   butt->setFlat(true);
   butt->setGeometry(0, 0, 30, 30);
   butt->setMaximumWidth(30);
+  butt->setPalette(newPalette);
   mainLayout.addWidget(butt);
   butt->setIconSize(butt->size());
 
   QPushButton* bText=new QPushButton(team.TeamName, this);
-  QPalette newPalette = palette();
-  newPalette.setColor(QPalette::Button, palette().color(backgroundRole()));
   bText->setPalette(newPalette);
   bText->setFlat(true);
   mainLayout.addWidget(bText);
