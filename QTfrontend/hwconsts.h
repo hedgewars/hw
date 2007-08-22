a995436be8ae7b85099d4b29f3ec260b81d785c3
unc0rr <unc0rr@gmail.com>
2007-08-22 16:37:51 +0000

    Oops, forgot file
    --HG--
    extra : convert_revision : svn%3A99beacd1-defe-0310-bff8-d3e2e4bee508/trunk%40602

---
+++ QTfrontend/hwconsts.h
@@ -0,0 +1,30 @@
+/*
+ * Hedgewars, a worms-like game
+ * Copyright (c) 2005-2007 Andrey Korotaev <unC0Rr@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+#include <QString>
+#include <QDir>
+#include <QStringList>
+
+extern QString * cProtoVer;
+extern QString * cDataDir;
+
+extern QDir * bindir;
+extern QDir * cfgdir;
+extern QDir * datadir;
+
+extern QStringList * Themes;
