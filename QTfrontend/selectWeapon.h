bb1d7e9236b1bfb81e38b5dcf80c958aa1540f73
displacer <disinbox@gmail.com>
2007-09-18 18:42:50 +0000

    new files added
    --HG--
    extra : convert_revision : svn%3A99beacd1-defe-0310-bff8-d3e2e4bee508/trunk%40619

---
+++ QTfrontend/selectWeapon.h
@@ -0,0 +1,11 @@
+#ifndef _SELECT_WEAPON_INCLUDED
+#define _SELECT_WEAPON_INCLUDED
+
+#include <QWidget>
+
+class SelWeaponWidget : public QWidget
+{
+  Q_OBJECT
+};
+
+#endif // _SELECT_WEAPON_INCLUDED
