89e53f3f4fc4d70414ec9853379a87590b2a20e9
unc0rr <unc0rr@gmail.com>
2009-01-29 14:49:47 +0000

    A bit better template
    --HG--
    extra : convert_revision : svn%3A99beacd1-defe-0310-bff8-d3e2e4bee508/trunk%401820

--- hedgewars/uLandTemplates.pas
+++ hedgewars/uLandTemplates.pas
@@ -44,24 +44,24 @@ type PPointArray = ^TPointArray;
 // The first 18 are in all 4 quadrants, the last 18 are in only the bottom 2
 const Template0Points: array[0..18] of TSDL_Rect =
       (
-       (x:  324; y: 1656; w:  196; h:  204),
-       (x:  224; y: 1496; w:  404; h:   60),
-       (x:  240; y: 1168; w:  464; h:  152),
-       (x:  876; y: 1136; w:  168; h:  348),
-       (x: 1204; y:  956; w:  148; h:  700),
-       (x: 1516; y:  952; w:  192; h:  664),
-       (x: 1808; y:  960; w:  328; h:  496),
-       (x: 2292; y:  992; w:  184; h:  492),
-       (x: 2664; y: 1116; w:  196; h:  340),
-       (x: 3004; y: 1008; w:  176; h:  480),
-       (x: 3260; y: 1268; w:  120; h:  348),
-       (x: 3476; y: 1360; w:  208; h:  448),
-       (x: 3268; y: 1856; w:  192; h:   96),
-       (x: 2876; y: 1564; w:  204; h:  380),
-       (x: 2240; y: 1648; w:  344; h:  324),
-       (x: 1584; y: 1696; w:  440; h:  300),
-       (x:  892; y: 1752; w:  324; h:  184),
-       (x:  576; y: 1876; w:   16; h:   28),
+       (x:  324; y:  756; w:  196; h:  204),
+       (x:  224; y:  596; w:  404; h:   60),
+       (x:  240; y:  268; w:  464; h:  152),
+       (x:  876; y:  236; w:  168; h:  348),
+       (x: 1204; y:   56; w:  148; h:  700),
+       (x: 1516; y:   52; w:  192; h:  664),
+       (x: 1808; y:   60; w:  328; h:  496),
+       (x: 2292; y:   92; w:  184; h:  492),
+       (x: 2664; y:  216; w:  196; h:  340),
+       (x: 3004; y:  108; w:  176; h:  480),
+       (x: 3260; y:  368; w:  120; h:  348),
+       (x: 3476; y:  460; w:  208; h:  448),
+       (x: 3268; y:  906; w:  192; h:   96),
+       (x: 2876; y:  664; w:  204; h:  310),
+       (x: 2240; y:  748; w:  344; h:  224),
+       (x: 1584; y:  796; w:  440; h:  250),
+       (x:  892; y:  852; w:  324; h:  184),
+       (x:  576; y:  976; w:   16; h:   28),
        (x: NTPX; y:    0; w:    1; h:    1)
       );
       Template0FPoints: array[0..0] of TPoint =
@@ -79,8 +79,8 @@ const EdgeTemplates: array[0..0] of TEdgeTemplate =
         BasePointsCount: Succ(High(Template0Points));
         FillPoints: @Template0FPoints;
         FillPointsCount: Succ(High(Template0FPoints));
-        BezierizeCount: 2;
-        RandPassesCount: 10;
+        BezierizeCount: 4;
+        RandPassesCount: 12;
         TemplateHeight: 1024; TemplateWidth: 4096;
         canMirror: true; canFlip: false; isNegative: true; canInvert: false;
        )
