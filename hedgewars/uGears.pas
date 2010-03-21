3df838871f6f97f9e502a86fdd4a96aefcd1baff
palewolf <mail@carlosvives.es>
2010-03-21 22:26:12 +0000

    Fix 16px offset on previous commit
    --HG--
    extra : convert_revision : svn%3A99beacd1-defe-0310-bff8-d3e2e4bee508/trunk%403089

--- hedgewars/uGears.pas
+++ hedgewars/uGears.pas
@@ -1593,7 +1593,7 @@ while Gear<>nil do
 
            gtGrave: DrawTextureF(PHedgehog(Gear^.Hedgehog)^.Team^.GraveTex, 1, hwRound(Gear^.X) + WorldDx, hwRound(Gear^.Y) + WorldDy, (GameTicks shr 7) and 7, 1, 32, 32);
 
-             gtUFO: DrawRotatedF(sprUFO, hwRound(Gear^.X) -16 + WorldDx, hwRound(Gear^.Y) - 16 + WorldDy, Gear^.Tag, 0, DxDy2Angle(Gear^.dY, Gear^.dX));
+             gtUFO: DrawRotatedF(sprUFO, hwRound(Gear^.X) + WorldDx, hwRound(Gear^.Y) + WorldDy, Gear^.Tag, 0, DxDy2Angle(Gear^.dY, Gear^.dX));
 
       gtPickHammer: DrawSprite(sprPHammer, hwRound(Gear^.X) - 16 + WorldDx, hwRound(Gear^.Y) - 50 + LongInt(((GameTicks shr 5) and 1) * 2) + WorldDy, 0);
             gtRope: DrawRope(Gear);
