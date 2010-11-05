5a8fd2323a426f5a4f656b12656e8e9d78289543
dragonfly <CoheedFTW@hotmail.co.uk>
2010-11-05 20:06:57 +0100

    new maps: ShoppaKing and TrophyRace

--- share/hedgewars/Data/Maps/TrophyRace/map.lua
+++ share/hedgewars/Data/Maps/TrophyRace/map.lua
@@ -1,63 +1,33 @@
 -- Hedgewars - Roperace for 2+ Players
 
 local caption = {
-	["en"] = "ShoppaKing Map Three: TrophyRace",
-	["sv"] = "ShoppaKing bana tre: TrophyRace",
-	["pl"] = "ShoppaKing: TrophyRace",
-	["es"] = "ShoppaKing, tercer mapa: TrophyRace"
-	}
-
-local subcaption = {
-	["en"] = "Made by Wolfmarc and Dragonfly",
-	["sv"] = "Skapad av Wolfmarc och Dragonfly",
-	["pl"] = "Zrobione przez Wolfmarca i Dragonfly",
-	["es"] = "Por Wolfmarc y Dragonfly"
+	["en"] = "TrophyRace",
 	}
 
 local goal = {
 	["en"] = "Use your rope to get from start to finish as fast as you can!",
-	["sv"] = "Använd ditt rep för att ta dig från start till mål så fort du kan!",
-	["pl"] = "Użyj liny by jak najszybciej dotrzec od startu do mety",
-	["es"] = "¡Usa tu cuerda para llegar a la salida lo más rápido que puedas!"
 	}
 
 local done = {
 	["en"] = "You've reached the goal!| |Time: ",
-	["sv"] = "Du är i mål!| |Tid: ",
-	["pl"] = "Dotarłeś do celu!| |Czas: ",
-	["es"] = "¡Has llegado a la meta!| |Tiempo: "
 	}
+
 local eliminated = {
 	["en"] = "Eliminating worst hedgehog this round...| |%s is OUT!",
-	["sv"] = "Eliminerar sämsta igelkotten den här omgången...| |%s är UTE!",
-	["pl"] = "Eliminujemy najgorszego jeża tej rundy...| |%s ODPADŁ!",
-	["es"] = "Eliminando al peor erizo de esta ronda...| |¡%s está FUERA!"
 	}
 
 local newbesttime = {
 	["en"] = "NEW fastest lap: ",
-	["sv"] = "NY snabbaste tid: ",
-	["pl"] = "NOWE najszybsze okrążenie: ",
-	["es"] = "NUEVA vuelta rápida: "
 	}
 
 local oldbesttime = {
 	["en"] = "Fastest lap: ",
-	["sv"] = "Snabbaste tid: ",
-	["pl"] = "Najszybsze okrążenie: ",
-	["es"] = "Vuelta rápida: "
 	}
 local bestclantimes = {
 	["en"] = "Best laps per team: ",
-	["sv"] = "Bästa tider per lag: ",
-	["pl"] = "Najszybsze okrążenie drużyny: ",
-	["es"] = "Mejores tiempos por equipo: "
 	}
 local clantime = {
 	["en"] = "Team %d: ",
-	["sv"] = "Lag %d: ",
-	["pl"] = "Drużyna %d: ",
-	["es"] = "Equipo %d"
 	}
 
 	local function loc(text)
@@ -120,7 +90,7 @@ function onGameInit()
 end
 
 function onGameStart()
-	ShowMission(loc(caption), loc(subcaption), loc(goal), -amRope, 0)
+	ShowMission(loc(caption), "", loc(goal), -amRope, 0)
 	started = true
 	p=1820
 	for i = 0, numhhs - 1 do
@@ -204,7 +174,7 @@ function onGameTick()
 				hscore = hscore .. "|" .. string.format(loc(clantime), i+1) .. tt
 			end
 			
-			ShowMission(loc(caption), loc(subcaption), loc(done) .. (ttime / 1000) .. " s" .. hscore, 0, 0)
+			ShowMission(loc(caption), "", loc(done) .. (ttime / 1000) .. " s" .. hscore, 0, 0)
 			TurnTimeLeft = 0
 		end
 	end
