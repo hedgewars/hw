2fab76189f2dc41abcaa39b7e923584717573462
koda <vittorio.giovara@gmail.com>
2010-11-12 15:20:54 +0100

    yay i'm free to break things again!

--- hedgewars/uWorld.pas
+++ hedgewars/uWorld.pas
@@ -149,14 +149,14 @@ ScreenFadeValue:= sfMax;
 ScreenFadeSpeed:= 1;
 
 // modified mine timers?
-if cMinesTime <> 3000 then
+if cMinesTime <> 3 then
     begin
     if cMinesTime = 0 then
         g:= AddGoal(g, gfAny, gidNoMineTimer)
     else if cMinesTime < 0 then
         g:= AddGoal(g, gfAny, gidRandomMineTimer)
     else
-        g:= AddGoal(g, gfAny, gidMineTimer, cMinesTime div 1000);
+        g:= AddGoal(g, gfAny, gidMineTimer, cMinesTime);
     end;
 
 // if the string has been set, show it for (default timeframe) seconds
