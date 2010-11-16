b86b3948d0e3c712eb3f41b0ed2841eca78cc405
jose1711 <jose1711@gmail.com>
2010-11-16 20:55:39 +0100

    updated/added slovak translations of lua files

--- share/hedgewars/Data/Missions/Campaign/01#Boot Camp.lua
+++ share/hedgewars/Data/Missions/Campaign/01#Boot Camp.lua
@@ -10,7 +10,8 @@ teamnames[0] = {
 	["sv"] = "Blodiga nybörjare",
 	["es"] = "Reclutas",
 	["pt_PT"] = "Recrutas",
-	["pt_BR"] = "Maldito Recrutas"
+	["pt_BR"] = "Maldito Recrutas",
+	["sk"] = "Regruti"
 }
 
 teamnames[1] = {
@@ -20,7 +21,8 @@ teamnames[1] = {
 	["sv"] = "Instruktör",
 	["es"] = "Instructores",
 	["pt_PT"] = "Instrutores",
-	["pt_BR"] = "Instrutores"
+	["pt_BR"] = "Instrutores",
+	["sk"] = "Inštruktori"
 }
 
 hognames[0] = {
@@ -30,7 +32,8 @@ hognames[0] = {
 	["sv"] = "Joker",
 	["es"] = "Joker",
 	["pt_PT"] = "Joker",
-	["pt_BR"] = "Comediante"
+	["pt_BR"] = "Comediante",
+	["sk"] = "Komediant"
 }
 
 hognames[1] = {
@@ -40,7 +43,8 @@ hognames[1] = {
 	["sv"] = "Harthog",
 	["es"] = "Harthog",
 	["pt_PT"] = "Harthog",
-	["pt_BR"] = "Harthog"
+	["pt_BR"] = "Harthog",
+	["sk"] = "Harthog"
 }
 
 local caption = {
@@ -50,7 +54,8 @@ local caption = {
 	["sv"] = "Grundutbildning",
 	["es"] = "Campamento militar",
 	["pt_PT"] = "Acampamento militar",
-	["pt_BR"] = "Acampamento Militar"
+	["pt_BR"] = "Acampamento Militar",
+	["sk"] = "Výcvikový tábor"
 
 }
 
@@ -61,7 +66,8 @@ local subcaption = {
 	["sv"] = "Följ instruktioner!",
 	["es"] = "¡Sigue las órdenes!",
 	["pt_PT"] = "Segue as instruções!",
-	["pt_BR"] = "Siga as ordens!"
+	["pt_BR"] = "Siga as ordens!",
+	["sk"] = "Postupujte podľa inštrukcií!"
 }
 
 local goals = {}
@@ -73,7 +79,8 @@ goals[0] = {
 	["sv"] = "Lyssna på din instruktör och gör som han säger!",
 	["es"] = "¡Escucha atentamente a tu instructor de maniobras y sigue sus órdenes!",
 	["pt_PT"] = "Ouve atentamente o teu Instrutor e segue as suas ordens!",
-	["pt_BR"] = "Escute seu instrutor de escavação e siga seu líder"
+	["pt_BR"] = "Escute seu instrutor de escavação e siga seu líder",
+	["sk"] = "Počúvajte vášho inštruktora výcviku a vykonávajte jeho rozkazy!"
 }
 
 goals[1] = {
@@ -83,7 +90,8 @@ goals[1] = {
 	["sv"] = "Förstör målet för att avklara uppdraget!",
 	["es"] = "¡Destruye el objetivo para completar esta misión!",
 	["pt_PT"] = "Destrói o alvo para completar esta missão!",
-	["pt_BR"] = "Destrua o alvo para concluir a missão"
+	["pt_BR"] = "Destrua o alvo para concluir a missão",
+	["sk"] = "Pre ukončenie misie zneškodnite cieľ!"
 }
 
 goals[2] = {
@@ -93,7 +101,8 @@ goals[2] = {
 	["sv"] = "Brilliant! Du har klarat av grundutbildningen!",
 	["es"] = "¡Excelente! ¡Has completado la maniobra de hoy!",
 	["pt_PT"] = "Excelente! Completaste o treino para recrutas!",
-	["pt_BR"] = "Excelente! Você completou a manobra"
+	["pt_BR"] = "Excelente! Você completou a manobra",
+	["pt_BR"] = "Výborne! Prešli ste výcvikovým táborom!"
 }
 
 local failed = {
@@ -103,7 +112,8 @@ local failed = {
 	["sv"] = "Du har misslyckats! Följ instruktionerna och sjut endast på målen!",
 	["es"] = "¡Has fallado! ¡Sigue las instrucciones y destruye únicamente el objetivo!",
 	["pt_PT"] = "Falhaste! Segue as instruções e destrói apenas o alvo!",
-	["pt_BR"] = "Você falhou! Siga as instruções e atire somente no alvo"
+	["pt_BR"] = "Você falhou! Siga as instruções e atire somente no alvo",
+	["sk"] = "Prehrali ste! Nasledujte inštrukcie a strieľajte iba na cieľ!"
 }
 
 local drill = {}
@@ -115,7 +125,8 @@ drill[0] = {
 	["sv"] = "Dåså, din mask!",
 	["es"] = "¡Muy bien, escoria!",
 	["pt_PT"] = "Bom trabalho verme!",
-	["pt_BR"] = "Parabéns escória"
+	["pt_BR"] = "Parabéns escória",
+	["sk"] = "Počúvaj, ty červík!"
 }
 
 drill[1] = {
@@ -125,7 +136,8 @@ drill[1] = {
 	["sv"] = "Visa att du inte är så värdelös!",
 	["es"] = "Demuéstrame que no eres tan inútil como pareces.",
 	["pt_PT"] = "Prova-me que não és tão inútil como pareces.",
-	["pt_BR"] = "Mostre que você não é um inútil."
+	["pt_BR"] = "Mostre que você não é um inútil.",
+	["sk"] = "Dokáž, že nie si na svete úplne zbytočným."
 }
 
 drill[2] = {
@@ -135,7 +147,8 @@ drill[2] = {
 	["sv"] = "Använd [vänster] för att gå åt vänster!",
 	["es"] = "¡Aprieta [izquierda] para desplazarte a la izquierda!",
 	["pt_PT"] = "Carrega [esquerda] para te moveres para a esquerda!",
-	["pt_BR"] = "Use [esquerda] para mover para a esquerda"
+	["pt_BR"] = "Use [esquerda] para mover para a esquerda",
+	["sk"] = "Stlačte [vľavo] pre pohyb doľava"
 }
 
 drill[3] = {
@@ -145,7 +158,8 @@ drill[3] = {
 	["sv"] = "Bra! Använd nu [höger] för att gå tillbaka!",
 	["es"] = "¡Muy bien! ¡Ahora presiona [derecha] para volver!",
 	["pt_PT"] = "Muito bem! Agora pressiona [direita] para voltar!",
-	["pt_BR"] = "Bom! Agora use [direita] para voltar"
+	["pt_BR"] = "Bom! Agora use [direita] para voltar",
+	["sk"] = "Dobre! Teraz použite [vpravo] a vráťte sa naspäť!"
 }
 
 drill[4] = {
@@ -155,7 +169,8 @@ drill[4] = {
 	["sv"] = "Utmärkt!",
 	["es"] = "¡Excelente!",
 	["pt_PT"] = "Excelente!",
-	["pt_BR"] = "Excelente!"
+	["pt_BR"] = "Excelente!",
+	["sk"] = "Výborne!"
 }
 
 drill[5] = {
@@ -165,7 +180,8 @@ drill[5] = {
 	["sv"] = "Hoppa nu åt vänster med hjälp av [enter]!",
 	["es"] = "¡Ahora salta hacia la izquierda usando [intro]!",
 	["pt_PT"] = "Agora salta para a esquerda pressionando [retrocesso]!",
-	["pt_BR"] = "Agora pule para a esquerda usando [return"
+	["pt_BR"] = "Agora pule para a esquerda usando [return]!",
+	["sk"] = "Teraz skočte doľava stlačením [enter]!"
 }
 
 drill[6] = {
@@ -175,7 +191,8 @@ drill[6] = {
 	["sv"] = "Använd [upp] och [ner] för att sikta!",
 	["es"] = "Presiora [arriba] y [abajo] para apuntar.",
 	["pt_PT"] = "Pressiona [cima] e [baixo] para apontar.",
-	["pt_BR"] = "Pressione [acima] e [abaixo] para mirar."
+	["pt_BR"] = "Pressione [acima] e [abaixo] para mirar.",
+	["sk"] = "Stlačte [hore] a [dole] pre mierenie."
 }
 
 drill[7] = {
@@ -185,7 +202,8 @@ drill[7] = {
 	["sv"] = "Håll ner [mellanslag] för att få kraft och släpp för att skjuta!",
 	["es"] = "Mantén presionada la [barra espaciadora] para controlar la fuerza del disparo y suéltala para disparar.",
 	["pt_PT"] = "Mantem a [barra de espaços] pressionada para controlar a força do tiro e larga-a para disparar.",
-	["pt_BR"] = "Mantenha pressionado [espaço] para aumentar a força do seu tiro e solte para atirar"
+	["pt_BR"] = "Mantenha pressionado [espaço] para aumentar a força do seu tiro e solte para atirar",
+	["sk"] = "Držte stlačený [medzerník] pre nabíjanie, jeho uvoľnením vystrelíte."
 }
 
 drill[8] = {
@@ -195,7 +213,8 @@ drill[8] = {
 	["sv"] = "Förstör målet för att avsluta din grundutbildning!",
 	["es"] = "¡Destruye el objetivo para completar el entrenamiento básico!",
 	["pt_PT"] = "Destrói o alvo para completar o treino básico!",
-	["pt_BR"] = "Destrua o alvo para terminar o treino básico"
+	["pt_BR"] = "Destrua o alvo para terminar o treino básico",
+	["sk"] = "Zneškodnite cieľ na ukončenie základného výcviku!"
 }
 
 local function loc(text)
