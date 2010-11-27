142d6f96fe2564666550780cb523e7ae7e0d4b04
unc0rr <unc0rr@gmail.com>
2010-11-27 23:57:31 +0300

    Small testing app for DrawMap scene

---
+++ tools/drawMapTest/drawMapTest.pro
@@ -0,0 +1,11 @@
+# -------------------------------------------------
+# Project created by QtCreator 2010-11-27T22:18:20
+# -------------------------------------------------
+TARGET = drawMapTest
+TEMPLATE = app
+SOURCES += main.cpp \
+    mainwindow.cpp \
+    drawmapscene.cpp
+HEADERS += mainwindow.h \
+    drawmapscene.h
+FORMS += mainwindow.ui
---
+++ tools/drawMapTest/mainwindow.h
@@ -0,0 +1,25 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+protected:
+    void changeEvent(QEvent *e);
+
+private:
+    Ui::MainWindow *ui;
+
+    virtual void resizeEvent(QResizeEvent * event);
+};
+
+#endif // MAINWINDOW_H
