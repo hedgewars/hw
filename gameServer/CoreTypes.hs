91c2b4f51f07cbf93d5b2add8426f9e8676813fc
unc0rr <unc0rr@gmail.com>
2011-03-13 15:20:07 +0300

    - Remove not needed Show instances  - Introduce BanInfo data type

--- gameServer/CoreTypes.hs
+++ gameServer/CoreTypes.hs
@@ -39,9 +39,6 @@ data ClientInfo =
         teamsInGame :: Word
     }
 
-instance Show ClientInfo where
-    show ci = " nick: " ++ unpack (nick ci) ++ " host: " ++ unpack (host ci)
-
 instance Eq ClientInfo where
     (==) = (==) `on` clientSocket
 
@@ -64,11 +61,6 @@ data TeamInfo =
         hedgehogs :: [HedgehogInfo]
     }
 
-instance Show TeamInfo where
-    show ti = "owner: " ++ unpack (teamowner ti)
-            ++ "name: " ++ unpack (teamname ti)
-            ++ "color: " ++ unpack (teamcolor ti)
-
 data RoomInfo =
     RoomInfo
     {
@@ -88,11 +80,6 @@ data RoomInfo =
         mapParams :: Map.Map B.ByteString B.ByteString,
         params :: Map.Map B.ByteString [B.ByteString]
     }
-
-instance Show RoomInfo where
-    show ri = ", players: " ++ show (playersIn ri)
-            ++ ", ready: " ++ show (readyPlayers ri)
-            ++ ", teams: " ++ show (teams ri)
 
 newRoom :: RoomInfo
 newRoom =
@@ -145,8 +132,6 @@ data ServerInfo =
         serverConfig :: Maybe Conf
     }
 
-instance Show ServerInfo where
-    show _ = "Server Info"
 
 newServerInfo :: Chan CoreMessage -> Chan DBQuery -> Maybe Conf -> ServerInfo
 newServerInfo =
@@ -184,13 +169,6 @@ data CoreMessage =
     | TimerAction Int
     | Remove ClientIndex
 
-instance Show CoreMessage where
-    show (Accept _) = "Accept"
-    show (ClientMessage _) = "ClientMessage"
-    show (ClientAccountInfo {}) = "ClientAccountInfo"
-    show (TimerAction _) = "TimerAction"
-    show (Remove _) = "Remove"
-
 type MRnC = MRoomsAndClients RoomInfo ClientInfo
 type IRnC = IRoomsAndClients RoomInfo ClientInfo
 
@@ -212,3 +190,8 @@ data ShutdownThreadException = ShutdownThreadException String
 instance Show ShutdownThreadException where
     show (ShutdownThreadException s) = s
 instance Exception ShutdownThreadException
+
+data BanInfo =
+    BanByIP String UTCTime
+    | BanByNickname String UTCTime
+    deriving (Show, Read)
