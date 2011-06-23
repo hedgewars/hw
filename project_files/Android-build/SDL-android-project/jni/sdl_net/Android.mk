804dfa7fbd80b98b54d497d980d0ed26856ae9fc
xeli <xeli@xelification.com>
2011-06-23 14:30:04 +0200

    Added sdl_net from the unofficial android port: http://www.anddev.org/sdl_port_for_android_sdk-ndk_16-t9218.html
    --HG--
    branch : hedgeroid

---
+++ project_files/Android-build/SDL-android-project/jni/sdl_net/Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := SDL_net
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH) $(LOCAL_PATH)/../SDL/include $(LOCAL_PATH)/include
+LOCAL_CFLAGS := -O3
+
+LOCAL_CPP_EXTENSION := .cpp
+
+LOCAL_SRC_FILES := $(notdir $(wildcard $(LOCAL_PATH)/*.c))
+
+LOCAL_SHARED_LIBRARIES := SDL
+LOCAL_STATIC_LIBRARIES :=
+LOCAL_LDLIBS :=
+
+include $(BUILD_SHARED_LIBRARY)
+
