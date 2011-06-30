32f52e1d49de4b9163c87bf3428b3c38b10f7fd2
nemo <nemo@m8y.org>
2011-06-30 18:20:41 -0400

    Sweep all pixels at 0 or 1 neighbours, whether damaged or not.  Intended to handled output of Smooth, might be nice to cut down on the counting somehow though.

--- hedgewars/uLandGraphics.pas
+++ hedgewars/uLandGraphics.pas
@@ -715,9 +715,7 @@ else
    yy:= Y div 2;
    end;
 pixelsweep:= ((Land[Y, X] and $FF00) = 0) and (LandPixels[yy, xx] <> 0);
-if ((((Land[Y, X] and lfDamaged) <> 0) or 
-    (((Land[Y, X] and lfBasic) <> 0) and (LandPixels[yy,xx] <> 0) and ((LandPixels[yy,xx] and AMask) shl AShift < 255)))
-    and ((Land[Y, X] and lfIndestructible) = 0)) or pixelsweep then
+if ((Land[Y, X] > 255) and ((Land[Y, X] and lfIndestructible) = 0)) or pixelsweep then
     begin
     c:= 0;
     for i:= -1 to 1 do
@@ -740,8 +738,8 @@ if ((((Land[Y, X] and lfDamaged) <> 0) or
                     else if Land[ny, nx] > 255 then inc(c);
                     end
                 end;
-
-    if c < 4 then // 0-3 neighbours
+    if (c < 2) or
+       ((c < 4) and (((Land[Y, X] and lfDamaged) <> 0) or pixelsweep)) then
         begin
         if ((Land[Y, X] and lfBasic) <> 0) and not disableLandBack then
             LandPixels[yy, xx]:= LandBackPixel(X, Y)
