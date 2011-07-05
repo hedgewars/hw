aa939422376f204d5450a46a651d3cb0cd4706de
xeli <xeli@xelification.com>
2011-07-05 18:27:18 +0200

    Added download functionality and changed some icons
    --HG--
    branch : hedgeroid

---
+++ project_files/Android-build/SDL-android-project/src/org/hedgewars/mobile/DownloadThread.java
@@ -0,0 +1,13 @@
+package org.hedgewars.mobile;
+
+public class DownloadThread extends Thread{
+	
+	public DownloadThread(){
+		
+	}
+	
+	public void run(){
+		
+	}
+
+}
---
+++ project_files/Android-build/SDL-android-project/src/org/hedgewars/mobile/MainActivity.java
@@ -0,0 +1,39 @@
+package org.hedgewars.mobile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	Button downloader, startGame;
+	
+	public void onCreate(Bundle sis){
+		super.onCreate(sis);
+		setContentView(R.layout.main);
+		
+		downloader = (Button)findViewById(R.id.downloader);
+		startGame = (Button)findViewById(R.id.startGame);
+		
+		downloader.setOnClickListener(downloadClicker);
+		startGame.setOnClickListener(startGameClicker);
+	}
+	
+	
+	
+	private OnClickListener downloadClicker = new OnClickListener(){
+		public void onClick(View v){
+			startActivityForResult(new Intent(getApplicationContext(), DownloadActivity.class), 0);
+		}
+	};
+
+	private OnClickListener startGameClicker = new OnClickListener(){
+		public void onClick(View v){
+			startActivity(new Intent(getApplicationContext(), SDLActivity.class));
+		}
+	};
+	
+}
