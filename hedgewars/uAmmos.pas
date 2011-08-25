59aa1c6ac3aee04b037adc13a9c4e61bf93c06ff
nemo <nemo@m8y.org>
2011-08-24 23:37:03 -0400

    another silly mistake

--- hedgewars/uAmmos.pas
+++ hedgewars/uAmmos.pas
@@ -184,7 +184,8 @@ var cnt: LongWord;
     a: PAmmo;
 begin
 a:= GetAmmoEntry(Hedgehog, ammo);
-if (a^.AmmoType = amNothing) or (a^.Count <> AMMO_INFINITE) then
+cnt:= a^.Count;
+if (a^.AmmoType = amNothing) or (cnt <> AMMO_INFINITE) then
     begin
     inc(cnt, Ammoz[ammo].NumberInCase);
     AddAmmo(Hedgehog, ammo, cnt)
