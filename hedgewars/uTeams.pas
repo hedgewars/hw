0b2beb2b4fde5d2cd59a5aba0fa537bb726c7093
unc0rr <unc0rr@gmail.com>
2011-09-22 17:19:39 +0400

    Fix sheepluva's merge

--- hedgewars/uTeams.pas
+++ hedgewars/uTeams.pas
@@ -427,12 +427,17 @@ begin
 with Team do
     for i:= 0 to cMaxHHIndex do
         with Hedgehogs[i] do
+            begin
+            if Hedgehogs[i].GearHidden <> nil then
+                RestoreHog(@Hedgehogs[i]);
+
             if Gear <> nil then
                 begin
                 Gear^.Invulnerable:= false;
                 Gear^.Damage:= Gear^.Health;
                 Gear^.State:= (Gear^.State or gstHHGone) and not gstHHDriven
                 end
+            end
 end;
 
 procedure chAddHH(var id: shortstring);
