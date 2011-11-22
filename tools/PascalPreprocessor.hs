558cf66ddf00d9f58dd8e61281a0cfe13a004a6d
unc0rr <unc0rr@gmail.com>
2011-11-22 22:48:02 +0300

    Preprocessor strips comments

--- tools/PascalPreprocessor.hs
+++ tools/PascalPreprocessor.hs
@@ -5,6 +5,14 @@ import Control.Monad.IO.Class
 import System.IO
 import qualified Data.Map as Map
 
+
+-- comments are removed
+comment = choice [
+        char '{' >> notFollowedBy (char '$') >> manyTill anyChar (try $ char '}') >> return ""
+        , (try $ string "(*") >> manyTill anyChar (try $ string "*)") >> return ""
+        , (try $ string "//") >> manyTill anyChar (try newline) >> return "\n"
+        ]
+
 preprocess :: String -> IO String
 preprocess fn = do
     r <- runParserT (preprocessFile fn) Map.empty "" ""
@@ -20,14 +28,20 @@ preprocess fn = do
         f <- liftIO (readFile fn)
         setInput f
         preprocessor
+        
     preprocessor, codeBlock, switch :: ParsecT String (Map.Map String String) IO String
+    
     preprocessor = chainl codeBlock (return (++)) ""
+    
     codeBlock = choice [
             switch
-            --, comment
+            , comment
             , char '\'' >> many (noneOf "'") >>= \s -> char '\'' >> return ('\'' : s ++ "'")
-            , many1 $ noneOf "{'"
+            , many1 $ noneOf "{'/("
+            , char '/' >> notFollowedBy (char '/') >> return "/"
+            , char '(' >> notFollowedBy (char '*') >> return "("
             ]
+            
     switch = do
         try $ string "{$"
         s <- choice [
@@ -35,6 +49,7 @@ preprocess fn = do
             , unknown
             ]
         return s
+        
     include = do
         try $ string "INCLUDE"
         spaces
