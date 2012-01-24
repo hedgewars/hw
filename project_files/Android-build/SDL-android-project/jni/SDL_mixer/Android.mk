a97570c7b6009d59f9627c1fac965a4e213046b6
xeli <xeli@xelification.com>
2012-01-24 02:47:51 +0100

    update the core/android/SDL_android.cpp fix some includes in Android.mks, use a different host to download some libs, sdl doesn't have up to date tarballs..

---
+++ project_files/Android-build/SDL-android-project/jni/SDL_mixer/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := SDL_mixer
+
+LOCAL_C_INCLUDES := \
+        $(LOCAL_PATH)/../../../../../misc/libtremor \
+	$(LOCAL_PATH)/.. \
+	$(LOCAL_PATH)/../SDL/include \
+	$(LOCAL_PATH)/../mikmod/include \
+
+LOCAL_CFLAGS := -DWAV_MUSIC -DOGG_MUSIC -DOGG_USE_TREMOR -DMOD_MUSIC
+
+LOCAL_SRC_FILES := $(notdir $(filter-out %/playmus.c %/playwave.c, $(wildcard $(LOCAL_PATH)/*.c)))
+
+LOCAL_SHARED_LIBRARIES := SDL mikmod
+LOCAL_STATIC_LIBRARIES := tremor
+
+include $(BUILD_SHARED_LIBRARY)
--- project_files/Android-build/download_libs.sh
+++ project_files/Android-build/download_libs.sh
@@ -7,12 +7,12 @@ download_and_zip (){
     mv SDL-android-project/jni/tmp/* SDL-android-project/jni/$2 #move the tmp dir to the jni directory
     rm tmp.zip #remove old tmp dir
 }
-download_and_zip http://www.libsdl.org/projects/SDL_image/libs/jpegsr8d.zip jpeg
-download_and_zip http://www.libsdl.org/projects/SDL_image/libs/lpng157.zip png
+download_and_zip http://www.xelification.com/tmp/jpeg.zip jpeg
+download_and_zip http://www.xelification.com/tmp/png.zip png
 download_and_zip http://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12.zip SDL_image
 download_and_zip http://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-1.2.12.zip SDL_mixer
 download_and_zip http://www.xelification.com/tmp/mikmod.zip mikmod #temporary url since the libsdl.org site doesn't work at the moment
-download_and_zip http://www.libsdl.org/projects/SDL_net/release/SDL_net-1.2.8.zip SDL_net
+download_and_zip http://www.xelification.com/tmp/SDL_net.zip SDL_net
 download_and_zip http://www.libsdl.org/projects/SDL_ttf/release/SDL_ttf-2.0.11.zip SDL_ttf
 download_and_zip http://www.libsdl.org/tmp/SDL-1.3.zip SDL
 
