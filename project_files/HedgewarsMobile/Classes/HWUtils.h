6cf804bba3c6c4c84ebae3b5beb8c8c480a99017
koda <vittorio.giovara@gmail.com>
2012-02-17 18:23:36 +0100

    the most important commit of the year

--- project_files/HedgewarsMobile/Classes/HWUtils.h
+++ project_files/HedgewarsMobile/Classes/HWUtils.h
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * File created on 01/10/2011.
+ * File created on 01/10/2012.
  */
 
 
--- project_files/HedgewarsMobile/Classes/MissionTrainingViewController.m
+++ project_files/HedgewarsMobile/Classes/MissionTrainingViewController.m
@@ -1,6 +1,6 @@
 /*
  * Hedgewars-iOS, a Hedgewars port for iOS devices
- * Copyright (c) 2009-2011 Vittorio Giovara <vittorio.giovara@gmail.com>
+ * Copyright (c) 2009-2012 Vittorio Giovara <vittorio.giovara@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * File created on 03/10/2011.
+ * File created on 03/10/2012.
  */
 
 
