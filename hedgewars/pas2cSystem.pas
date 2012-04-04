17edce2f13a48807d689392490cc72226d9a0f10
unc0rr <unc0rr@gmail.com>
2012-04-04 18:00:46 +0400

    Finish the toughest part of the converter. Now it knows types of everything, so could correctly recognize bitwise operators and type convertions.

--- hedgewars/pas2cSystem.pas
+++ hedgewars/pas2cSystem.pas
@@ -14,6 +14,7 @@ type
     GLInt = integer;
     GLUInt = integer;
     gl_unsigned_byte = integer;
+    Int = integer;
 
     pointer = pointer;
     PChar = pointer;
@@ -46,7 +47,9 @@ type
 
 var 
     false, true: boolean;
+    
     write, writeLn, read, readLn: procedure;
+    
     StrLen, ord, Succ, Pred : function : integer;
     inc, dec, Low, High, Lo, Hi : function : integer;
     odd, even : function : boolean;
@@ -60,19 +63,21 @@ var
     trunc, round : function : integer;
     Abs, Sqr : function : integer;
 
-    StrPas, FormatDateTime, copy, delete, str, pos : function : shortstring;
+    StrPas, FormatDateTime, copy, delete, str, pos, trim : function : shortstring;
 
-    assign, rewrite, reset, flush, BlockWrite, close : procedure;
+    assign, rewrite, reset, flush, BlockWrite, BlockRead, close : procedure;
     IOResult : function : integer;
     exit, break, halt, continue : procedure;
     TextFile, file : Handle;
     FileMode : integer;
-    eof : function : boolean;
+    FileExists, DirectoryExists, eof : function : boolean;
+    ExtractFileName : function : string;
+    exitcode : integer;
     
     ParamCount : function : integer;
     ParamStr : function : string;
 
-    Sqrt, ArcTan2, pi, cos, sin : function : float;
+    sqrt, arctan2, pi, cos, sin, power : function : float;
 
     TypeInfo, GetEnumName : function : shortstring;
 
@@ -91,13 +96,30 @@ var
     glcolor4ub, gl_texture_wrap_s, gltexparameteri,
     gl_texture_wrap_t, gl_texture_min_filter,
     gl_linear, gl_texture_mag_filter, glgentextures,
-    gldeletetextures, glreadpixels : procedure;
+    gldeletetextures, glreadpixels, glclearcolor,
+    gl_line_strip, gldeleterenderbuffersext,
+    gldeleteframebuffersext, glext_loadextension,
+    gl_max_texture_size, glgetintegerv, gl_renderer,
+    glgetstring, gl_vendor, gl_version, glgenframebuffersext,
+    glbindframebufferext, glgenrenderbuffersext,
+    glbindrenderbufferext, glrenderbufferstorageext,
+    glframebufferrenderbufferext, glframebuffertexture2dext,
+    gl_framebuffer_ext, gl_depth_component, 
+    gl_depth_attachment_ext, gl_renderbuffer_ext, gl_rgba8,
+    gl_color_attachment0_ext, gl_modelview, gl_blend,
+    gl_src_alpha, gl_one_minus_src_alpha,  
+    gl_perspective_correction_hint, gl_fastest,
+    gl_dither, gl_vertex_array, gl_texture_coord_array,
+    glviewport, glloadidentity, glmatrixmode, glhint,
+    glblendfunc, glenableclientstate, gl_color_buffer_bit,
+    glclear : procedure;
 
     TThreadId : function : integer;
     BeginThread, ThreadSwitch : procedure;
     InterlockedIncrement, InterlockedDecrement : procedure;
     
     random : function : integer;
+    randomize : procedure;
     
     Assigned : function : boolean;
     
