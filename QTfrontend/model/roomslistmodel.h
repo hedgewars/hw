9ebe0fe55408bd9c54baa896f18a3b34df09e42c
sheepluva <sheepyluva@gmail.com>
2012-04-30 20:12:43 +0200

    add missing copyright headers to roomslistmodel

--- QTfrontend/model/roomslistmodel.h
+++ QTfrontend/model/roomslistmodel.h
@@ -1,5 +1,28 @@
-#ifndef ROOMSLISTMODEL_H
-#define ROOMSLISTMODEL_H
+/*
+ * Hedgewars, a free turn based strategy game
+ * Copyright (c) 2004-2012 Andrey Korotaev <unC0Rr@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+/**
+ * @file
+ * @brief RoomsListModel class definition
+ */
+
+#ifndef HEDGEWARS_ROOMSLISTMODEL_H
+#define HEDGEWARS_ROOMSLISTMODEL_H
 
 #include <QAbstractTableModel>
 #include <QStringList>
@@ -28,4 +51,4 @@ private:
     QStringList roomInfo2RoomRecord(const QStringList & info);
 };
 
-#endif // ROOMSLISTMODEL_H
+#endif // HEDGEWARS_ROOMSLISTMODEL_H
