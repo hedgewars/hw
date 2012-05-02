34aaa429557daf77f6615465e2411531dbca3d8e
sheepluva <sheepyluva@gmail.com>
2012-05-02 19:36:23 +0200

    allow F5 magic to refresh demos/saves list too
    --HG--
    extra : rebase_source : e02c3367619519aff7fd92ddc679626996ebc001

--- QTfrontend/ui/page/pageplayrecord.cpp
+++ QTfrontend/ui/page/pageplayrecord.cpp
@@ -16,6 +16,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  */
 
+#include "pageplayrecord.h"
+
 #include <QFont>
 #include <QGridLayout>
 #include <QPushButton>
@@ -26,7 +28,8 @@
 #include <QInputDialog>
 
 #include "hwconsts.h"
-#include "pageplayrecord.h"
+
+#include "DataManager.h"
 
 QLayout * PagePlayDemo::bodyLayoutDefinition()
 {
@@ -61,6 +64,7 @@ void PagePlayDemo::connectSignals()
 {
     connect(BtnRenameRecord, SIGNAL(clicked()), this, SLOT(renameRecord()));
     connect(BtnRemoveRecord, SIGNAL(clicked()), this, SLOT(removeRecord()));
+    connect(&DataManager::instance(), SIGNAL(updated()), this, SLOT(refresh()));
 }
 
 PagePlayDemo::PagePlayDemo(QWidget* parent) : AbstractPage(parent)
@@ -102,6 +106,14 @@ void PagePlayDemo::FillFromDir(RecordType rectype)
         DemosList->item(i)->setIcon(recType == RT_Demo ? QIcon(":/res/file_demo.png") : QIcon(":/res/file_save.png"));
     }
 }
+
+
+void PagePlayDemo::refresh()
+{
+    if (this->isVisible());
+        FillFromDir(recType);
+}
+
 
 void PagePlayDemo::renameRecord()
 {
