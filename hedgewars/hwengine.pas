9a84148bf15a705d617382fecbd826c348658cc1
xeli <xeli@xelification.com>
2012-05-17 20:19:35 +0200

    remove ProcessKdb references all together, removed movecursor() because it doesnt seem to do anything

--- hedgewars/hwengine.pas
+++ hedgewars/hwengine.pas
@@ -82,7 +82,6 @@ begin
         gsConfirm, gsGame:
             begin
             DrawWorld(Lag); // never place between ProcessKbd and DoGameTick - bugs due to /put cmd and isCursorVisible
-//            ProcessKbd;
             DoGameTick(Lag);
             ProcessVisualGears(Lag);
             end;
