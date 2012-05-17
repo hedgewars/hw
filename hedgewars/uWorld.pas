30defd380624dab08e9de42054af81002f571750
xeli <xeli@xelification.com>
2012-05-17 16:51:54 +0200

    android typo/sabotage

--- hedgewars/uWorld.pas
+++ hedgewars/uWorld.pas
@@ -1753,7 +1753,7 @@ end;
 
 procedure SetUtilityWidgetState(ammoType: TAmmoType);
 begin
-{$IFDEF TOUCH_INTERFACE}
+{$IFDEF USE_TOUCH_INTERFACE}
 if(ammoType = amNothing)then
     ammoType:= CurrentHedgehog^.CurAmmoType;
 
