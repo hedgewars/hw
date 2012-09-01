a5748277a8cbdbe0da41a720e97aa953e41b6b33
sheepluva <sheepyluva@gmail.com>
2012-09-01 10:40:22 +0200

    fix parsing of 'if': identifiers can also contain underscores

--- tools/PascalParser.hs
+++ tools/PascalParser.hs
@@ -438,7 +438,7 @@ phrase = do
     return o
 
 ifBlock = do
-    try $ string "if" >> notFollowedBy alphaNum
+    try $ string "if" >> notFollowedBy (alphaNum <|> char '_')
     comments
     e <- expression
     comments
