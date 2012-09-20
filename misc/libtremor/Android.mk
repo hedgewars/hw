d1ef97b6325b7aa10f6cf4ec84e2dec65dbde24c
koda <vittorio.giovara@gmail.com>
2012-09-21 01:29:33 +0200

    blindly trying to fix the android build

--- misc/libtremor/Android.mk
+++ misc/libtremor/Android.mk
@@ -7,21 +7,12 @@ LOCAL_MODULE := tremor
 LOCAL_CFLAGS := -I$(LOCAL_PATH) -DHAVE_ALLOCA_H
 
 LOCAL_SRC_FILES =    \
-        tremor/bitwise.c    \
-        tremor/block.c      \
-        tremor/codebook.c   \
-        tremor/floor0.c     \
-        tremor/floor1.c     \
-        tremor/framing.c    \
-        tremor/info.c       \
-        tremor/mapping0.c   \
-        tremor/mdct.c       \
-        tremor/registry.c   \
-        tremor/res012.c     \
-        tremor/sharedbook.c \
-        tremor/synthesis.c  \
-        tremor/vorbisfile.c \
-        tremor/window.c     
+    tremor/bitwise.c             tremor/info.c             tremor/codebook.c \
+    tremor/dsp.c                 tremor/mapping0.c \
+    tremor/floor0.c              tremor/mdct.c \
+    tremor/floor1.c              tremor/misc.c \
+    tremor/floor_lookup.c        tremor/res012.c \
+    tremor/framing.c             tremor/vorbisfile.c
 
 include $(BUILD_STATIC_LIBRARY)
 
