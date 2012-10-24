048ce13fe28bcf36ba88c19a977228ed86c4afef
koda <vittorio.giovara@gmail.com>
2012-10-24 10:18:42 +0100

    download SDL dlls if not present
    --HG--
    extra : rebase_source : 6c798050032b5129db7fd6cf2c3a7a51422c0f07

--- tools/build_windows.bat
+++ tools/build_windows.bat
@@ -8,13 +8,20 @@ set PATH=%PATH%;%PASCAL%
 set CURRDIR="%CD%"
 cd ..
 
-echo Copying the DLLs...
-REM xcopy /d/y %CD%\misc\winutils\bin\* .
-xcopy /d/y %QTDIR%\QtCore4.dll bin
-xcopy /d/y %QTDIR%\QtGui4.dll bin
-xcopy /d/y %QTDIR%\QtNetwork4.dll bin
-xcopy /d/y %QTDIR%\libgcc_s_dw2-1.dll bin
-xcopy /d/y %QTDIR%\mingwm10.dll bin
+echo Fetching all DLLs...
+for %%G in (QtCore4 QtGui4 QtNetwork4 libgcc_s_dw2-1 mingwm10) do (
+    xcopy /d/y %QTDIR%\%%G.dll bin\
+)
+
+if not exist %CD%\misc\winutils\bin\ mkdir %CD%\misc\winutils\bin\
+if not exist %CD%\misc\winutils\bin\SDL.dll cscript %CD%\tools\w32DownloadUnzip.vbs http://www.libsdl.org/release/SDL-1.2.15-win32.zip %CD%\misc\winutils\bin
+if not exist %CD%\misc\winutils\bin\SDL_image.dll cscript %CD%\tools\w32DownloadUnzip.vbs http://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12-win32.zip %CD%\misc\winutils\bin
+if not exist %CD%\misc\winutils\bin\SDL_net.dll cscript %CD%\tools\w32DownloadUnzip.vbs http://www.libsdl.org/projects/SDL_net/release/SDL_net-1.2.8-win32.zip %CD%\misc\winutils\bin
+if not exist %CD%\misc\winutils\bin\SDL_mixer.dll cscript %CD%\tools\w32DownloadUnzip.vbs http://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-1.2.12-win32.zip %CD%\misc\winutils\bin
+if not exist %CD%\misc\winutils\bin\SDL_ttf.dll cscript %CD%\tools\w32DownloadUnzip.vbs  http://www.libsdl.org/projects/SDL_ttf/release/SDL_ttf-2.0.11-win32.zip %CD%\misc\winutils\bin
+
+xcopy /d/y %CD%\misc\winutils\bin\*.dll bin
+xcopy /d/y %CD%\misc\winutils\bin\*.txt bin
 
 ::setting up the environment...
 call %QTDIR%\qtenv2.bat
