d32c980761d573d261944b6788c1e0fb2d483134
koda <vittorio.giovara@gmail.com>
2012-10-29 05:12:24 +0100

    this commit is a no-op

--- hedgewars/uInputHandler.pas
+++ hedgewars/uInputHandler.pas
@@ -220,7 +220,7 @@ KeyNames[5]:= 'wheeldown';
 for i:= 6 to cKeyMaxIndex do
     begin
     s:= shortstring(sdl_getkeyname(i));
-    //WriteLnToConsole(IntToStr(i) + ': ' + s + ' ' + IntToStr(cKeyMaxIndex));
+    //WriteLnToConsole('uInputHandler - ' + IntToStr(i) + ': ' + s + ' ' + IntToStr(cKeyMaxIndex));
     if s = 'unknown key' then KeyNames[i]:= ''
     else 
         begin
