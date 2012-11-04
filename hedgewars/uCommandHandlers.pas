f1e1477ab60f0214afc8b0ef2c7035fcd7446ee3
nemo <nemo@m8y.org>
2012-11-04 17:47:44 -0500

    Simplify ammo menu conditions so it shows up in more states.  Should fix issue #465.  The part w/ sliding in uworld is main #465 problem.
    --HG--
    branch : 0.9.18
    extra : source : d00378a37dde633d44b063bd702ded87902cf18b

--- hedgewars/uCommandHandlers.pas
+++ hedgewars/uCommandHandlers.pas
@@ -598,9 +598,9 @@ else
 
             if bShowAmmoMenu then
                 bShowAmmoMenu:= false
-            else if ((Gear^.State and (gstAttacking or gstAttacked)) <> 0)
+            else if not(CurrentTeam^.Extdriven) and (((Gear^.State and (gstAttacking or gstAttacked)) <> 0)
             or ((MultiShootAttacks > 0) and ((Ammoz[CurAmmoType].Ammo.Propz and ammoprop_NoRoundEnd) = 0))
-            or ((Gear^.State and gstHHDriven) = 0) then
+            or ((Gear^.State and gstHHDriven) = 0)) then
                 begin
                 end
             else
