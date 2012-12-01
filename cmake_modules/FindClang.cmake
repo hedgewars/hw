db63b5e133cda231135898b1b55c8b3afcdf9ecf
Bryan Dunsmore <dunsmoreb@gmail.com>
2012-11-30 21:28:40 -0600

    Move version check of Clang to CMakeLists.
    --HG--
    branch : webgl

--- cmake_modules/FindClang.cmake
+++ cmake_modules/FindClang.cmake
@@ -13,13 +13,6 @@ IF (CLANG_EXECUTABLE)
 
     STRING(REGEX MATCH "[0-9]+\\.[0-9]+" CLANG_VERSION_LONG "${CLANG_VERSION_FULL}")
     STRING(REGEX REPLACE "([0-9]+\\.[0-9]+)" "\\1" CLANG_VERSION "${CLANG_VERSION_LONG}")
-
-    # Required that LLVM/Clang version is >= 3.0
-    IF (CLANG_VERSION VERSION_GREATER 3.0 OR CLANG_VERSION VERSION_EQUAL 3.0)
-        MESSAGE(STATUS "Found CLANG: ${CLANG_EXECUTABLE} (version ${CLANG_VERSION})")
-    ELSE()
-        MESSAGE(FATAL_ERROR "Necessary LLVM/Clang version not found (version >= 3.0 required)")
-    ENDIF()
 ELSE()
     MESSAGE(FATAL_ERROR "No LLVM/Clang compiler found (required for engine_c target)")
 ENDIF()
