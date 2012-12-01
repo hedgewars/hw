0f5e458ea6665dc30e9849a3b2e9471f6135c04e
Bryan Dunsmore <dunsmoreb@gmail.com>
2012-12-01 15:40:10 -0600

    Move noexecstack flags to Freepascal module and refactor.

--- cmake_modules/FindFreepascal.cmake
+++ cmake_modules/FindFreepascal.cmake
@@ -1,18 +1,34 @@
 # Load Freepascal
-IF (FPC)
-    SET(FPC_EXECUTABLE ${FPC})
-ELSE()
-    FIND_PROGRAM(FPC_EXECUTABLE
+if (FPC)
+    set(FPC_EXECUTABLE ${FPC})
+else()
+    find_program(FPC_EXECUTABLE
         NAMES fpc
         PATHS /opt/local/bin /usr/local/bin /usr/bin)
-ENDIF()
+endif()
 
 # Check Freepascal version
-IF (FPC_EXECUTABLE)
-    EXEC_PROGRAM(${FPC_EXECUTABLE} ARGS "-v" OUTPUT_VARIABLE FPC_VERSION_FULL)
+if (FPC_EXECUTABLE)
+    exec_program(${FPC_EXECUTABLE} ARGS "-v" OUTPUT_VARIABLE FPC_VERSION_FULL)
 
-    STRING(REGEX MATCH "[0-9]+\\.[0-9]+" FPC_VERSION_LONG "${FPC_VERSION_FULL}")
-    STRING(REGEX REPLACE "([0-9]+\\.[0-9]+)" "\\1" FPC_VERSION "${FPC_VERSION_LONG}")
-ELSE()
-    MESSAGE(FATAL_ERROR "Freepascal not found (required for hedgewars)")
-ENDIF()
+    string(REGEX MATCH "[0-9]+\\.[0-9]+" FPC_VERSION_LONG "${FPC_VERSION_FULL}")
+    string(REGEX REPLACE "([0-9]+\\.[0-9]+)" "\\1" FPC_VERSION "${FPC_VERSION_LONG}")
+else()
+    message(FATAL_ERROR "Freepascal not found (required for hedgewars)")
+endif()
+
+# Check for noexecstack flag support
+set(NOEXECSTACK_FLAGS "-k-z" "-knoexecstack")
+file(WRITE ${EXECUTABLE_OUTPUT_PATH}/checkstack.pas "begin end.")
+
+execute_process(COMMAND ${FPC_EXECUTABLE} ${NOEXECSTACK_FLAGS} checkstack.pas
+    WORKING_DIRECTORY ${EXECUTABLE_OUTPUT_PATH}
+    RESULT_VARIABLE TEST_NOEXECSTACK
+    OUTPUT_QUIET ERROR_QUIET)
+
+if (TEST_NOEXECSTACK)
+    set(NOEXECSTACK_FLAGS "")
+    message(STATUS "Checking whether linker supports noexecstack flag -- no")
+else()
+    message(STATUS "Checking whether linker supports noexecstack flag -- yes")
+endif()
