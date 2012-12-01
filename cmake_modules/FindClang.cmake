3a1c37b5516bb2e7a41d2ee8be761616d2eb6e30
Bryan Dunsmore <dunsmoreb@gmail.com>
2012-12-01 15:52:36 -0600

    Refactor Clang module.
    --HG--
    branch : webgl

--- cmake_modules/FindClang.cmake
+++ cmake_modules/FindClang.cmake
@@ -1,20 +1,20 @@
 # Load LLVM/Clang
-IF (CLANG)
-    SET(CLANG_EXECUTABLE ${CLANG})
-ELSE()
-    FIND_PROGRAM(CLANG_EXECUTABLE
+if (CLANG)
+    set(CLANG_EXECUTABLE ${CLANG})
+else()
+    find_program(CLANG_EXECUTABLE
         NAMES clang-mp-3.2 clang-mp-3.1 clang-mp-3.0 clang
         PATHS /opt/local/bin /usr/local/bin /usr/bin)
-ENDIF()
+endif()
 
 # Check LLVM/Clang version
-IF (CLANG_EXECUTABLE)
-    EXEC_PROGRAM(${CLANG_EXECUTABLE} ARGS "-v" OUTPUT_VARIABLE CLANG_VERSION_FULL)
+if (CLANG_EXECUTABLE)
+    exec_program(${CLANG_EXECUTABLE} ARGS "-v" OUTPUT_VARIABLE CLANG_VERSION_FULL)
 
-    STRING(REGEX MATCH "[0-9]+\\.[0-9]+" CLANG_VERSION_LONG "${CLANG_VERSION_FULL}")
-    STRING(REGEX REPLACE "([0-9]+\\.[0-9]+)" "\\1" CLANG_VERSION "${CLANG_VERSION_LONG}")
-ELSE()
-    MESSAGE(FATAL_ERROR "No LLVM/Clang compiler found (required for engine_c target)")
-ENDIF()
+    string(REGEX MATCH "[0-9]+\\.[0-9]+" CLANG_VERSION_LONG "${CLANG_VERSION_FULL}")
+    string(REGEX REPLACE "([0-9]+\\.[0-9]+)" "\\1" CLANG_VERSION "${CLANG_VERSION_LONG}")
+else()
+    message(FATAL_ERROR "No LLVM/Clang compiler found (required for engine_c target)")
+endif()
 
-SET(CMAKE_C_COMPILER ${CLANG_EXECUTABLE})
+set(CMAKE_C_COMPILER ${CLANG_EXECUTABLE})
