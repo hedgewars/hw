ff814302c177f5dd1b8425ac2cda47656f948b0d
unc0rr <unc0rr@gmail.com>
2013-02-02 00:47:34 +0400

    Fix server crasher
    --HG--
    extra : rebase_source : c431d379a6487f48a4f83ef6f1704395dd208edf

--- gameServer/Actions.hs
+++ gameServer/Actions.hs
@@ -430,7 +430,7 @@ processAction CheckRegistered = do
     uid <- client's clUID
     -- allow multiple checker logins
     haveSameNick <- liftM (not . null . tail . filter (\c -> (not $ isChecker c) && caseInsensitiveCompare (nick c) n)) allClientsS
-    if haveSameNick && (not checker) then
+    if (not checker) && haveSameNick then
         if p < 38 then
             processAction $ ByeClient $ loc "Nickname is already in use"
             else
