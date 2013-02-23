0c8a613ecdf6deadbc5e00018a3d9d56ca404ccf
koda <vittorio.giovara@gmail.com>
2013-02-23 18:49:45 +0100

    blindly updated Android build files for new physfs
    --HG--
    branch : physfslayer

--- misc/Android.mk
+++ misc/Android.mk
@@ -4,4 +4,6 @@ LOCAL_PATH := MISC_DIR
 include $(MISC_DIR)/libfreetype/Android.mk
 include $(MISC_DIR)/liblua/Android.mk
 include $(MISC_DIR)/libtremor/Android.mk
-include $(MISC_DIR)/physfs/Android.mk
+include $(MISC_DIR)/libphysfs/Android.mk
+include $(MISC_DIR)/libphyslayer/Android.mk
+
