f4e3cbbcbc92e467880acd8724fb050791ba6af0
nemo <nemo@m8y.org>
2013-03-06 13:03:11 -0500

    skip frozen hogs in switch

--- hedgewars/uAI.pas
+++ hedgewars/uAI.pas
@@ -414,7 +414,7 @@ if (PGear(Me)^.State and gstAttacked) = 0 then
             // find another hog in team
             repeat
                 itHedgehog:= Succ(itHedgehog) mod CurrentTeam^.HedgehogsNumber;
-            until (itHedgehog = currHedgehogIndex) or (CurrentTeam^.Hedgehogs[itHedgehog].Gear <> nil);
+            until (itHedgehog = currHedgehogIndex) or ((CurrentTeam^.Hedgehogs[itHedgehog].Gear <> nil) and (CurrentTeam^.Hedgehogs[itHedgehog].Effects[heFrozen]=0));
 
 
             inc(switchesNum);
