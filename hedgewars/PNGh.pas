af69fabebfb29199dbe7c730a0cc675204100ba9
koda <vittorio.giovara@gmail.com>
2013-03-07 10:11:07 +0100

    link png library in code on osx

--- hedgewars/PNGh.pas
+++ hedgewars/PNGh.pas
@@ -29,9 +29,13 @@ uses png;
     {$DEFINE cdecl attribute(cdecl)}
 {$ENDIF}
 
+{$IFDEF DARWIN}
+    {$linklib png}
+{$ENDIF}
+
 const
     // Constants for libpng, they are not defined in png unit.
-    // We actually don't need all of them.
+    // We actually do not need all of them.
 
     // These describe the color_type field in png_info.
     // color type masks
