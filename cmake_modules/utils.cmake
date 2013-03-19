f240bcdeba2715ace6b2f4c937d195ba028ec57b
koda <vittorio.giovara@gmail.com>
2013-03-19 09:53:12 +0100

    utility macros for adding compiler flags
    --HG--
    branch : cmake_pascal

--- cmake_modules/utils.cmake
+++ cmake_modules/utils.cmake
@@ -28,3 +28,12 @@ endmacro(find_package_or_disable_msg _PKG_NAME _VAR_NAME _MSG)
 
 #TODO: find_package_or_bundle
 
+
+macro(add_flag_append _VAR_NAME _FLAG)
+    set(${_VAR_NAME} "${${_VAR_NAME}} ${_FLAG}")
+endmacro(add_flag_append _VAR_NAME _FLAG)
+
+macro(add_flag_prepend _VAR_NAME _FLAG)
+    set(${_VAR_NAME} "_FLAG ${${_VAR_NAME}}")
+endmacro(add_flag_prepend _VAR_NAME _FLAG)
+
