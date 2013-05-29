43ae9a662770850164000b60b5173a8b87715735
unc0rr <unc0rr@gmail.com>
2013-05-29 15:12:06 -0400

    bump copyright year for Andrey entries

--- QTfrontend/team.h
+++ QTfrontend/team.h
@@ -1,6 +1,6 @@
 /*
  * Hedgewars, a free turn based strategy game
- * Copyright (c) 2004-2012 Andrey Korotaev <unC0Rr@gmail.com>
+ * Copyright (c) 2004-2013 Andrey Korotaev <unC0Rr@gmail.com>
  * Copyright (c) 2007 Igor Ulyanov <iulyanov@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
