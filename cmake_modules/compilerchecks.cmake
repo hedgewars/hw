5fbb22c0efd862510990e0534185dec136d71379
koda <vittorio.giovara@gmail.com>
2013-06-08 01:56:23 +0200

    add relro linker tests (issue 663)

--- cmake_modules/compilerchecks.cmake
+++ cmake_modules/compilerchecks.cmake
@@ -12,6 +12,22 @@ if(HAVE_NOEXECSTACK)
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${CMAKE_REQUIRED_FLAGS}")
 endif()
 
+#check for full relro on ELF, Debian security
+set(CMAKE_REQUIRED_FLAGS "-Wl,-z,relro,-z,now")
+check_c_compiler_flag("" HAVE_RELROFULL)
+if(HAVE_RELROFULL)
+    list(APPEND pascal_flags "-k-z" "-krelro" "-k-z" "-know")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${CMAKE_REQUIRED_FLAGS}")
+else()
+    #if full relro is not available, try partial relro
+    set(CMAKE_REQUIRED_FLAGS "-Wl,-z,relro")
+    check_c_compiler_flag("" HAVE_RELROPARTIAL)
+    if(HAVE_RELROPARTIAL)
+        list(APPEND pascal_flags "-k-z" "-krelro")
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${CMAKE_REQUIRED_FLAGS}")
+    endif()
+endif()
+
 #check for ASLR on Windows Vista or later, requires binutils >= 2.20
 set(CMAKE_REQUIRED_FLAGS "-Wl,--nxcompat")
 check_c_compiler_flag("" HAVE_WINASLR)
