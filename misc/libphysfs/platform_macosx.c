59ac0edb8662e5bcd498cfd2fe1f2011115aa851
koda <vittorio.giovara@gmail.com>
2013-06-11 00:01:32 +0200

    libssp (required by stack-protector) is not available on 10.4
    --HG--
    extra : rebase_source : 655051a89c1cdbd0cef45b5b721fc090acf5db31

--- misc/libphysfs/platform_macosx.c
+++ misc/libphysfs/platform_macosx.c
@@ -44,6 +44,31 @@ void __eprintf (const char *string, const char *expression,
     fflush(stderr);
     abort();
 }
+
+
+/* apparently libssp is missing from 10.4 SDK
+   code from http://wiki.osdev.org/GCC_Stack_Smashing_Protector */
+void * __stack_chk_guard = NULL;
+
+void __stack_chk_guard_setup()
+{
+    unsigned char * p;
+    p = (unsigned char *) &__stack_chk_guard;
+
+    /* If you have the ability to generate random numbers in your kernel then use them,
+       otherwise for 32-bit code: *p = 0x00000aff; */
+    *p = random();
+}
+
+void __attribute__((noreturn)) __stack_chk_fail()
+{
+    /* put your panic function or similar in here */
+    unsigned char * vid = (unsigned char *)0xB8000;
+    vid[1] = 7;
+    for(;;)
+    vid[0]++;
+}
+
 #endif
 #endif /* __APPLE__ */
 
