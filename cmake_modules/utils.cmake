2e5a64eb3fb1db9184cd86f42ab613e117b29146
koda <vittorio.giovara@gmail.com>
2013-06-27 12:18:35 +0200

    restore the GLUT check

--- cmake_modules/utils.cmake
+++ cmake_modules/utils.cmake
@@ -12,7 +12,7 @@ macro(find_package_or_disable _PKG_NAME _VAR_NAME)
     find_package(${_PKG_NAME})
     string(TOUPPER ${_PKG_NAME} _PKG_NAME_UP)
     if(NOT ${_PKG_NAME_UP}_FOUND)
-        message(SEND_ERROR "Missing ${_PKG_NAME}! Rerun cmake with -D${_VAR_NAME}=1 to build without it.")
+        message(SEND_ERROR "Missing ${_PKG_NAME}! Rerun cmake with -D${_VAR_NAME}=1 to skip this error.")
     endif(NOT ${_PKG_NAME_UP}_FOUND)
 endmacro(find_package_or_disable _PKG_NAME _VAR_NAME)
 
