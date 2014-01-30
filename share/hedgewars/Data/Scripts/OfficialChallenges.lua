a1fa6f1c24018565d59b7154089c544dc1287317
unc0rr <unc0rr@gmail.com>
2014-01-30 16:08:05 +0400

    Official racer challenge #2

--- share/hedgewars/Data/Scripts/OfficialChallenges.lua
+++ share/hedgewars/Data/Scripts/OfficialChallenges.lua
@@ -1,5 +1,9 @@
 function detectMap()
-    if LandDigest == "M838018718Scripts/Multiplayer/Racer.lua" then
-        return("Racer Challenge #1")
+    if RopePercent == 100 and MinesNum == 0 then
+        if LandDigest == "M838018718Scripts/Multiplayer/Racer.lua" then
+            return("Racer Challenge #1")
+        elseif LandDigest == "M-490229244Scripts/Multiplayer/Racer.lua" then
+            return("Racer Challenge #2")
+        end
     end
 end
