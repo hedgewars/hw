831b11c971c245273f9ad84ff90e94315b32ccba
unc0rr <unc0rr@gmail.com>
2014-02-21 13:31:16 +0400

    Official challenge map #3 by 1937barcelona

--- share/hedgewars/Data/Scripts/OfficialChallenges.lua
+++ share/hedgewars/Data/Scripts/OfficialChallenges.lua
@@ -4,6 +4,8 @@ function detectMap()
             return("Racer Challenge #1")
         elseif LandDigest == "M-490229244Scripts/Multiplayer/Racer.lua" then
             return("Racer Challenge #2")
+        elseif LandDigest == "M806689586Scripts/Multiplayer/Racer.lua" then
+            return("Racer Challenge #3")
         end
     end
 end
