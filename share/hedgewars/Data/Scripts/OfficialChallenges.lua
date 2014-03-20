21d3e55417987358d5ec1d1ff1a9ed77a95a3b94
unc0rr <unc0rr@gmail.com>
2014-03-20 22:14:30 +0400

    Better detection

--- share/hedgewars/Data/Scripts/OfficialChallenges.lua
+++ share/hedgewars/Data/Scripts/OfficialChallenges.lua
@@ -1,11 +1,15 @@
 function detectMap()
     if RopePercent == 100 and MinesNum == 0 then
-        if LandDigest == "M838018718Scripts/Multiplayer/Racer.lua" then
-            return("Racer Challenge #1")
-        elseif LandDigest == "M-490229244Scripts/Multiplayer/Racer.lua" then
-            return("Racer Challenge #2")
-        elseif LandDigest == "M806689586Scripts/Multiplayer/Racer.lua" then
-            return("Racer Challenge #3")
+-- challenges with border
+        if GetGameFlag(gfBorder) then
+            if LandDigest == "M838018718Scripts/Multiplayer/Racer.lua" then
+                return("Racer Challenge #1")
+            elseif LandDigest == "M-490229244Scripts/Multiplayer/Racer.lua" then
+                return("Racer Challenge #2")
+            elseif LandDigest == "M806689586Scripts/Multiplayer/Racer.lua" then
+                return("Racer Challenge #3")
+        end
+-- challenges without border
         elseif LandDigest == "M-134869715Scripts/Multiplayer/Racer.lua" then
             return("Racer Challenge #4")
         end
