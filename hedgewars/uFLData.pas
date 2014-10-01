4575af63bd537df8b24007f758297c0f1f30b1e5
unc0rr <unc0rr@gmail.com>
2014-10-02 00:48:14 +0400

    Filter out garbage from themes list
    --HG--
    branch : qmlfrontend

--- hedgewars/uFLData.pas
+++ hedgewars/uFLData.pas
@@ -9,13 +9,49 @@ implementation
 uses uPhysFSLayer;
 
 function getThemesList: PPChar; cdecl;
+var list, res, tmp: PPChar;
+    i, size: Longword;
 begin
-    getThemesList:= pfsEnumerateFiles('Themes')
+    list:= pfsEnumerateFiles('Themes');
+    size:= 0;
+    tmp:= list;
+    while tmp^ <> nil do
+    begin
+        inc(size);
+        inc(tmp)
+    end;
+
+    res:= GetMem((3 + size) * sizeof(PChar));
+    res^:= PChar(list);
+    inc(res);
+    res^:= PChar(res + size + 2);
+    inc(res);
+
+    getThemesList:= res;
+
+    for i:= 1 to size do
+    begin
+        if pfsExists('/Themes/' + shortstring(list^) + '/icon.png') then
+        begin
+            res^:= list^;
+            inc(res)
+        end;
+
+        inc(list)
+    end;
+
+    res^:= nil
 end;
 
 procedure freeThemesList(list: PPChar); cdecl;
+var listEnd: PPChar;
 begin
-    pfsFreeList(list)
+    dec(list);
+    listEnd:= PPChar(list^);
+    dec(list);
+
+    pfsFreeList(PPChar(list^));
+    freeMem(list, (listEnd - list) * sizeof(PChar))
 end;
 
 function getThemeIcon(themeName: PChar; buffer: PChar; buflen: Longword): Longword; cdecl;
