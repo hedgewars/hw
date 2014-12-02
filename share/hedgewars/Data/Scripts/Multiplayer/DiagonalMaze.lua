6370d50d468d210683b5c6ddf6ef3c424ca18a7b
nemo <nemo@m8y.org>
2014-12-02 14:26:16 -0500

    Sheepluva thought this was worth adding.

---
+++ share/hedgewars/Data/Scripts/Multiplayer/DiagonalMaze.lua
@@ -0,0 +1,22 @@
+HedgewarsScriptLoad("/Scripts/Draw.lua")
+
+function onPreviewInit()
+onGameInit()
+end
+
+function onGameInit()
+    MapGen = mgDrawn
+    TemplateFilter = 0
+    for y = 48,2048,200 do
+       for x = 48,4048,200 do
+            if GetRandom(2) == 0 then
+                AddPoint(x,y,1)
+                AddPoint(x+200,y+200)
+            else
+                AddPoint(x,y+200,1)
+                AddPoint(x+200,y)
+            end
+        end
+    end
+    FlushPoints()
+end 
