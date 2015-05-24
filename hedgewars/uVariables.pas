48fd06bcfc7c4e30a59dddf7b01bfedcc5706d38
nemo <nemo@m8y.org>
2015-05-24 15:44:21 -0400

    change from default of 4 (like land mines) to 0, since it was breaking old scripts.

--- hedgewars/uVariables.pas
+++ hedgewars/uVariables.pas
@@ -2620,7 +2620,7 @@ begin
     AttackBar       := 0; // 0 - none, 1 - just bar at the right-down corner, 2 - from weapon
     cCaseFactor     := 5;  {0..9}
     cLandMines      := 4;
-    cAirMines       := 4;
+    cAirMines       := 0;
     cExplosives     := 2;
 
     GameState       := Low(TGameState);
