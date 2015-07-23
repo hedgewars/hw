1294790c610396dc31b7cd31f02ed7e464c1193d
unc0rr <unc0rr@gmail.com>
2015-07-23 19:51:14 +0300

    Don't allow too big messages from clients

--- gameServer/ClientIO.hs
+++ gameServer/ClientIO.hs
@@ -55,6 +55,7 @@ listenLoop sock chan ci = receiveWithBufferLoop B.empty
             unless (B.null recvBS) $ do
                 let (packets, newrecvBuf) = bs2Packets $ B.append recvBuf recvBS
                 forM_ packets sendPacket
+                when (B.length newrecvBuf > 128 * 1024) $ sendPacket ["QUIT", "Protocol violation"]
                 receiveWithBufferLoop $ B.copy newrecvBuf
 
         sendPacket packet = writeChan chan $ ClientMessage (ci, packet)
