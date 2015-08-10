0530fbcc9772869f0043cad1c2ff8a24afc72231
sheepluva <sheepyluva@gmail.com>
2015-08-10 03:53:11 +0200

    more copyright fixes

--- project_files/frontlib/net/netconn.c
+++ project_files/frontlib/net/netconn.c
@@ -1,7 +1,7 @@
 /*
  * Hedgewars, a free turn based strategy game
  * Copyright (c) 2006-2008 Igor Ulyanov <iulyanov@gmail.com>
- * Copyright (c) 2004-2014 Andrey Korotaev <unC0Rr@gmail.com>
+ * Copyright (c) 2004-2015 Andrey Korotaev <unC0Rr@gmail.com>
  * Copyright (c) 2012 Simeon Maxein <smaxein@googlemail.com>
  *
  * This program is free software; you can redistribute it and/or modify
