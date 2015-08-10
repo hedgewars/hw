2ab2c6f24e62a1209a54c759593cf4cc76420926
antonc27 <antonc27@mail.ru>
2015-08-10 22:44:51 +0200

    - Restore game functionality disabled, because it blocks main screen
    Note: Uncomment this after
    
    --HG--
    branch : ios-revival

--- project_files/HedgewarsMobile/Classes/MainMenuViewController.m
+++ project_files/HedgewarsMobile/Classes/MainMenuViewController.m
@@ -60,22 +60,22 @@
         [CreationChamber createFirstLaunch];
     }
 
-    // prompt for restoring any previous game
-    NSString *saveString = [userDefaults objectForKey:@"savedGamePath"];
-    if (saveString != nil && [saveString isEqualToString:@""] == NO && [[userDefaults objectForKey:@"saveIsValid"] boolValue]) {
-        if (self.restoreViewController == nil) {
-            NSString *xibName = [@"RestoreViewController-" stringByAppendingString:(IS_IPAD() ? @"iPad" : @"iPhone")];
-            RestoreViewController *restored = [[RestoreViewController alloc] initWithNibName:xibName bundle:nil];
-            if ([restored respondsToSelector:@selector(setModalPresentationStyle:)])
-                restored.modalPresentationStyle = UIModalPresentationFormSheet;
-            self.restoreViewController = restored;
-            [restored release];
-        }
-        [self performSelector:@selector(presentModalViewController:animated:) withObject:self.restoreViewController afterDelay:0.25];
-    } else {
-        // let's not prompt for rating when app crashed >_>
-        [Appirater appLaunched];
-    }
+//    // prompt for restoring any previous game
+//    NSString *saveString = [userDefaults objectForKey:@"savedGamePath"];
+//    if (saveString != nil && [saveString isEqualToString:@""] == NO && [[userDefaults objectForKey:@"saveIsValid"] boolValue]) {
+//        if (self.restoreViewController == nil) {
+//            NSString *xibName = [@"RestoreViewController-" stringByAppendingString:(IS_IPAD() ? @"iPad" : @"iPhone")];
+//            RestoreViewController *restored = [[RestoreViewController alloc] initWithNibName:xibName bundle:nil];
+//            if ([restored respondsToSelector:@selector(setModalPresentationStyle:)])
+//                restored.modalPresentationStyle = UIModalPresentationFormSheet;
+//            self.restoreViewController = restored;
+//            [restored release];
+//        }
+//        [self performSelector:@selector(presentModalViewController:animated:) withObject:self.restoreViewController afterDelay:0.25];
+//    } else {
+//        // let's not prompt for rating when app crashed >_>
+//        [Appirater appLaunched];
+//    }
 
     /*
     [ServerProtocolNetwork openServerConnection];
