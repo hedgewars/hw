6538dca345902faf619ab2daac487f1fa368b2f1
antonc27 <antonc27@mail.ru>
2015-08-14 01:55:42 +0200

    - MGSplitViewController updated to last version with iOS 8 fixes
    --HG--
    branch : ios-revival

--- project_files/HedgewarsMobile/Classes/MGSplitViewController/MGSplitCornersView.h
+++ project_files/HedgewarsMobile/Classes/MGSplitViewController/MGSplitCornersView.h
@@ -9,23 +9,23 @@
 #import <UIKit/UIKit.h>
 
 typedef enum _MGCornersPosition {
-    MGCornersPositionLeadingVertical    = 0, // top of screen for a left/right split.
-    MGCornersPositionTrailingVertical   = 1, // bottom of screen for a left/right split.
-    MGCornersPositionLeadingHorizontal  = 2, // left of screen for a top/bottom split.
-    MGCornersPositionTrailingHorizontal = 3  // right of screen for a top/bottom split.
+	MGCornersPositionLeadingVertical	= 0, // top of screen for a left/right split.
+	MGCornersPositionTrailingVertical	= 1, // bottom of screen for a left/right split.
+	MGCornersPositionLeadingHorizontal	= 2, // left of screen for a top/bottom split.
+	MGCornersPositionTrailingHorizontal	= 3  // right of screen for a top/bottom split.
 } MGCornersPosition;
 
 @class MGSplitViewController;
 @interface MGSplitCornersView : UIView {
-    float cornerRadius;
-    MGSplitViewController *splitViewController;
-    MGCornersPosition cornersPosition;
-    UIColor *cornerBackgroundColor;
+	float cornerRadius;
+	MGSplitViewController *__unsafe_unretained splitViewController;
+	MGCornersPosition cornersPosition;
+	UIColor *cornerBackgroundColor;
 }
 
 @property (nonatomic, assign) float cornerRadius;
-@property (nonatomic, assign) MGSplitViewController *splitViewController; // weak ref.
+@property (nonatomic, unsafe_unretained) MGSplitViewController *splitViewController; // weak ref.
 @property (nonatomic, assign) MGCornersPosition cornersPosition; // don't change this manually; let the splitViewController manage it.
-@property (nonatomic, retain) UIColor *cornerBackgroundColor;
+@property (nonatomic, strong) UIColor *cornerBackgroundColor;
 
 @end
