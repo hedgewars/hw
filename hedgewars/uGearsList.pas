62528a5d35459eb2ba14ade7caa23e2ba312fc03
nemo <nemo@m8y.org>
2015-09-13 16:24:37 -0400

    Oups. 6, not 12 - was testing

--- hedgewars/uGearsList.pas
+++ hedgewars/uGearsList.pas
@@ -466,7 +466,7 @@ case Kind of
                 gear^.Tag:= Y
                 end;
    gtAirAttack: begin
-                gear^.Health:= 12;
+                gear^.Health:= 6;
                 gear^.Damage:= 30;
                 gear^.Z:= cHHZ+2;
                 gear^.Tint:= gear^.Hedgehog^.Team^.Clan^.Color shl 8 or $FF
