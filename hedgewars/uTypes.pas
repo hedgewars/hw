0d3839d43e853ae87074f168cb3e102be5b83be1
antonc27 <antonc27@mail.ru>
2015-10-21 23:46:11 +0200

    - 'Press the target button to mark the target' message is localizable now
    --HG--
    branch : ios-revival

--- hedgewars/uTypes.pas
+++ hedgewars/uTypes.pas
@@ -456,7 +456,7 @@ type
             sidConfirm, sidSuddenDeath, sidRemaining, sidFuel, sidSync,
             sidNoEndTurn, sidNotYetAvailable, sidRoundSD, sidRoundsSD, sidReady,
             sidBounce1, sidBounce2, sidBounce3, sidBounce4, sidBounce5, sidBounce,
-            sidMute, sidAFK, sidAutoCameraOff, sidAutoCameraOn);
+            sidMute, sidAFK, sidAutoCameraOff, sidAutoCameraOn, sidPressTarget);
 
     // Events that are important for the course of the game or at least interesting for other reasons
     TEventId = (eidDied, eidDrowned, eidRoundStart, eidRoundWin, eidRoundDraw,
