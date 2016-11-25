4932708a0cd775ba50464d866c02c23788d795b7
Wuzzy <almikes@aol.com>
2016-11-25 14:06:00 +0100

    Remove team score messages in Knockball/Basketball because they're redundant

--- share/hedgewars/Data/Maps/Basketball/map.lua
+++ share/hedgewars/Data/Maps/Basketball/map.lua
@@ -2,8 +2,6 @@
 
 HedgewarsScriptLoad("/Scripts/Locale.lua")
 
-local score = {[0] = 0, [1] = 0, [2] = 0, [3] = 0, [4] = 0, [5] = 0}
-
 local started = false
 
 function onGameInit()
@@ -24,37 +22,9 @@ function onGameStart()
 	started = true
 end
 
-function onGameTick()
-end
-
 function onAmmoStoreInit()
 	SetAmmo(amBaseballBat, 9, 0, 0, 0)
 	SetAmmo(amSkip, 9, 0, 0, 0)
-end
-
-function onGearAdd(gear)
-end
-
-function onGearDelete(gear)
-	if not started then
-		return
-	end
-	if (GetGearType(gear) == gtHedgehog) and CurrentHedgehog ~= nil then
-		local clan = GetHogClan(CurrentHedgehog)
-		local s
-		if clan ~= nil then
-			if GetHogClan(CurrentHedgehog) ~= GetHogClan(gear) then
-				score[clan] = score[clan] + 1
-				s = string.format(loc("%s is out and Team %d|scored a point!| |Score:"), GetHogName(gear), clan + 1)
-			else
-				score[clan] = score[clan] - 1
-				s = string.format(loc("%s is out and Team %d|scored a penalty!| |Score:"), GetHogName(gear), clan + 1)
-			end
-			s = s .. " " .. score[0]
-			for i = 1, ClansCount - 1 do s = s .. " - " .. score[i] end
-			ShowMission(loc("Basketball"), loc("Not So Friendly Match"), s, -amBaseballBat, 0)
-		end
-	end
 end
 
 function onNewTurn()
--- share/hedgewars/Data/Maps/Knockball/map.lua
+++ share/hedgewars/Data/Maps/Knockball/map.lua
@@ -2,11 +2,7 @@
 
 HedgewarsScriptLoad("/Scripts/Locale.lua")
 
-local score = {[0] = 0, [1] = 0, [2] = 0, [3] = 0, [4] = 0, [5] = 0}
-
 local ball = nil
-
-local started = false
 
 function onGameInit()
 	GameFlags = gfSolidLand + gfInvulnerable + gfDivideTeams
@@ -22,7 +18,6 @@ end
 
 function onGameStart()
 	ShowMission(loc("Knockball"), loc("Not So Friendly Match"), loc("Bat balls at your enemies and|push them into the sea!"), -amBaseballBat, 0)
-	started = true
 end
 
 function onGameTick()
@@ -48,26 +43,8 @@ function onGearAdd(gear)
 end
 
 function onGearDelete(gear)
-	if not started then
-		return
-	end
 	if gear == ball then
 		ball = nil
-	elseif (GetGearType(gear) == gtHedgehog) and CurrentHedgehog ~= nil then
-		local clan = GetHogClan(CurrentHedgehog)
-		local s
-		if clan ~= nil then
-			if GetHogClan(CurrentHedgehog) ~= GetHogClan(gear) then
-				score[clan] = score[clan] + 1
-				s = string.format(loc("%s is out and Team %d|scored a point!| |Score:"), GetHogName(gear), clan + 1)
-			else
-				score[clan] = score[clan] - 1
-				s = string.format(loc("%s is out and Team %d|scored a penalty!| |Score:"), GetHogName(gear), clan + 1)
-			end
-			s = s .. " " .. score[0]
-			for i = 1, ClansCount - 1 do s = s .. " - " .. score[i] end
-			ShowMission(loc("Knockball"), loc("Not So Friendly Match"), s, -amBaseballBat, 0)
-		end
 	end
 end
 
