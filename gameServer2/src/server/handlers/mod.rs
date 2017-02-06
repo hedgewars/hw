f1ef0afe774f9c762a2e35394e51dd8016749fe4
unc0rr <unc0rr@gmail.com>
2017-02-06 18:15:29 +0300

    Fix handlers module

--- gameServer2/src/server/handlers/mod.rs
+++ gameServer2/src/server/handlers/mod.rs
@@ -8,6 +8,10 @@ use super::actions::Action::*;
 use protocol::messages::HWProtocolMessage;
 use protocol::messages::HWServerMessage::*;
 
+mod loggingin;
+mod lobby;
+mod inroom;
+
 pub fn handle(server: &mut HWServer, token: mio::Token, poll: &mio::Poll, message: HWProtocolMessage) {
     match message {
         HWProtocolMessage::Ping =>
@@ -16,19 +20,16 @@ pub fn handle(server: &mut HWServer, token: mio::Token, poll: &mio::Poll, messag
             server.react(token, poll, vec![ByeClient("User quit: ".to_string() + &msg)]),
         HWProtocolMessage::Quit(None) =>
             server.react(token, poll, vec![ByeClient("User quit".to_string())]),
-        HWProtocolMessage::Nick(nick) =>
-            if server.clients[token].room_id == None {
-                server.react(token, poll, vec![SendMe(Nick(&nick).to_raw_protocol())]);
-                server.clients[token].nick = nick;
-                server.react(token, poll, vec![CheckRegistered]);
-            },
-        HWProtocolMessage::Proto(proto) => {
-                server.clients[token].protocol_number = proto;
-                server.react(token, poll, vec![CheckRegistered]);
-        },
-        HWProtocolMessage::List => warn!("Deprecated LIST message received"),
         HWProtocolMessage::Malformed => warn!("Malformed/unknown message"),
         HWProtocolMessage::Empty => warn!("Empty message"),
-        _ => unimplemented!(),
+        _ => {
+            if !server.clients[token].room_id.is_some() {
+                loggingin::handle(server, token, poll, message);
+            } else if server.clients[token].room_id == Some(server.lobby_id) {
+                lobby::handle(server, token, poll, message);
+            } else {
+                inroom::handle(server, token, poll, message);
+            }
+        },
     }
 }
