4a501d21c56b4889a58bbca4ab4ceddcad157da5
nemo <nemo@m8y.org>
2017-04-10 09:05:16 -0400

    merge in patch used in arch/fedora - fixes a lazy physfs memcpy that was breaking upstream's stricter checking

--- misc/libphysfs/archiver_zip.c
+++ misc/libphysfs/archiver_zip.c
@@ -312,7 +312,8 @@ static int ZIP_seek(PHYSFS_Io *_io, PHYSFS_uint64 offset)
                 return 0;
 
             inflateEnd(&finfo->stream);
-            memcpy(&finfo->stream, &str, sizeof (z_stream));
+            inflateCopy(&finfo->stream, &str);
+            inflateEnd(&str);
             finfo->uncompressed_position = finfo->compressed_position = 0;
         } /* if */
 
