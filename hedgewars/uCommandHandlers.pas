8c010cb145fb084006075cac50cd5509147004cd
Wuzzy <almikes@aol.com>
2017-04-10 20:56:01 +0200

    Fix crash when trying to open ammo menu with current team hog gear = nil
    Also fixes crash in A Classic Fairytale, mission 2 when rightclicking at Brainiac intro sequence (#142)

--- hedgewars/uCommandHandlers.pas
+++ hedgewars/uCommandHandlers.pas
@@ -554,7 +554,7 @@ else
 
             if bShowAmmoMenu then
                 bShowAmmoMenu:= false
-            else if not(CurrentTeam^.Extdriven) and (((Gear^.State and (gstAttacking or gstAttacked)) <> 0)
+            else if not(CurrentTeam^.Extdriven) and ((Gear = nil) or ((Gear^.State and (gstAttacking or gstAttacked)) <> 0)
             or ((Gear^.State and gstHHDriven) = 0)) then
                 begin
                 end
