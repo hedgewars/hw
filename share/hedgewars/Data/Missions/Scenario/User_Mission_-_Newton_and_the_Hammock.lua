0afeb7dc55148cc7f9ce8eee950cb20d69d25124
Wuzzy <almikes@aol.com>
2017-05-04 13:36:04 +0200

    Fix in-game mission name display of Newton and the Hammock

--- share/hedgewars/Data/Missions/Scenario/User_Mission_-_Newton_and_the_Hammock.lua
+++ share/hedgewars/Data/Missions/Scenario/User_Mission_-_Newton_and_the_Hammock.lua
@@ -126,9 +126,9 @@ end
 function onGearDelete(gear)
 
 	if (gear == enemy) and (failed == false) then
-		ShowMission(loc("Newton's Hammock"), loc("MISSION SUCCESSFUL"), loc("Congratulations!"), 0, 0)
+		ShowMission(loc("Newton and the Hammock"), loc("MISSION SUCCESSFUL"), loc("Congratulations!"), 0, 0)
 	elseif gear == player then
-		ShowMission(loc("Newton's Hammock"), loc("MISSION FAILED"), loc("Oh no! Just try again!"), -amSkip, 0)
+		ShowMission(loc("Newton and the Hammock"), loc("MISSION FAILED"), loc("Oh no! Just try again!"), -amSkip, 0)
 	end
 
 end
