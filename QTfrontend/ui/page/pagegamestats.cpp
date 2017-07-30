3363410b9387b835fbdf04f4cf797e30bcdf9af2
nemo <nemo@m8y.org>
2017-07-30 19:06:57 -0400

    while we're tweaking this phrase, "with" feels awkward

--- QTfrontend/ui/page/pagegamestats.cpp
+++ QTfrontend/ui/page/pagegamestats.cpp
@@ -317,7 +317,7 @@ void PageGameStats::GameStats(char type, const QString & info)
         {
             int i = info.indexOf(' ');
             int num = info.left(i).toInt();
-            QString message = "<p><img src=\":/res/StatsMostSelfDamage.png\"> " + PageGameStats::tr("<b>%1</b> thought it's good to shoot their own hedgehogs with <b>%2</b> pts.", "", num).arg(info.mid(i + 1)).arg(num) + "</p>";
+            QString message = "<p><img src=\":/res/StatsMostSelfDamage.png\"> " + PageGameStats::tr("<b>%1</b> thought it's good to shoot their own hedgehogs for <b>%2</b> pts.", "", num).arg(info.mid(i + 1)).arg(num) + "</p>";
             AddStatText(message);
             break;
         }
