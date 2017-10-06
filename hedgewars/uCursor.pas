b60b458c103c82eed63247422d3c112379ec7364
Wuzzy <almikes@aol.com>
2017-10-06 17:36:03 +0200

    Less stupid resetPosition comment in uCursor.pas

--- hedgewars/uCursor.pas
+++ hedgewars/uCursor.pas
@@ -19,12 +19,11 @@ end;
 procedure resetPosition;
 begin
     // Move curser by 1px in case it's already centered.
-    // Due to switch to SDL2, the game camera in the Alpha for 0.9.23
-    // screwed up if the game started with the mouse already being
-    // centered.
-    // No big deal since this function is (so far) only called once.
-    // This fixes it, but we might have overlooked an SDL2-related
+    // The game camera in the Alpha for 0.9.23 screwed up if
+    // the game started with the mouse already being centered.
+    // This fixes it, but we might have overlooked a related
     // bug somewhere else.
+    // No big deal since this function is (so far) only called once.
     SDL_WarpMouse((cScreenWidth div 2) + 1, cScreenHeight div 2);
     SDL_WarpMouse(cScreenWidth div 2, cScreenHeight div 2);
 end;
