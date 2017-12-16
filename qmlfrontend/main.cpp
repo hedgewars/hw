0ab2c2ec66a940522ac90b57f381fbd70f176d66
unc0rr <unc0rr@gmail.com>
2017-12-16 23:26:13 +0100

    Add new qmlfrontend project template

---
+++ qmlfrontend/main.cpp
@@ -0,0 +1,15 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QGuiApplication app(argc, argv);
+
+  QQmlApplicationEngine engine;
+  engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+  if (engine.rootObjects().isEmpty())
+    return -1;
+
+  return app.exec();
+}
