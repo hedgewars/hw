df195991b589a693fe9d8b7a0169f04d47435472
Wuzzy <Wuzzy2@mail.ru>
2018-03-28 15:14:20 +0200

    CMake: Rework Haskell package detection, use ghc-pkg to detect packages

--- cmake_modules/FindGHC.cmake
+++ cmake_modules/FindGHC.cmake
@@ -1,9 +1,10 @@
 # - Try to find the Glasgow Haskell Compiler executable
 # Once done this will define
 #
-#  GHC_FOUND       - system has GHC
-#  GHC_VERSION     - GHC version
-#  GHC_EXECUTABLE  - GHC executable
+#  GHC_VERSION        - GHC version
+#  GHC_EXECUTABLE     - GHC executable
+#  GHC_PKG_VERSION    - ghc-pkg version
+#  GHC_PKG_EXECUTABLE - ghc-pkg executable
 #
 # Copyright (c) 2013, Vittorio Giovara <vittorio.giovara@gmail.com>
 #
@@ -13,6 +14,11 @@
 
 find_program(GHC_EXECUTABLE
     NAMES ghc
+    PATHS /opt/local/bin /usr/local/bin /usr/bin
+    )
+
+find_program(GHC_PKG_EXECUTABLE
+    NAMES ghc-pkg
     PATHS /opt/local/bin /usr/local/bin /usr/bin
     )
 
@@ -29,6 +35,22 @@ if (GHC_EXECUTABLE)
         string(REGEX MATCH "([0-9]+)" GHC_VERSION ${GHC_VERSION_OUTPUT})
     else()
         message(SEND_ERROR "Command \"${GHC_EXECUTABLE} -V\" failed with output: ${GHC_VERSION_ERROR}")
+    endif()
+endif()
+
+if (GHC_PKG_EXECUTABLE)
+    # check ghc-pkg version
+    execute_process(COMMAND ${GHC_PKG_EXECUTABLE} -V
+                    OUTPUT_VARIABLE GHC_PKG_VERSION_OUTPUT
+                    ERROR_VARIABLE GHC_PKG_VERSION_ERROR
+                    RESULT_VARIABLE GHC_PKG_VERSION_RESULT
+                    OUTPUT_STRIP_TRAILING_WHITESPACE
+                    )
+
+    if(${GHC_PKG_VERSION_RESULT} EQUAL 0)
+        string(REGEX MATCH "([0-9]+)" GHC_PKG_VERSION ${GHC_PKG_VERSION_OUTPUT})
+    else()
+        message(SEND_ERROR "Command \"${GHC_PKG_EXECUTABLE} -V\" failed with output: ${GHC_PKG_VERSION_ERROR}")
     endif()
 endif()
 
