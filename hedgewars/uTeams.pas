95f42663b31a9ad456cd1266e820df41756ec72e
Wuzzy <Wuzzy2@mail.ru>
2018-08-11 22:28:05 +0200

    More straightforward TotalRoundsPre counting if there's only 1 clan

--- hedgewars/uTeams.pas
+++ hedgewars/uTeams.pas
@@ -314,6 +314,8 @@ if PlacingHogs then
 if not PlacingHogs then
     if (TotalRoundsReal = -1) then
         TotalRoundsReal:= 0;
+    if (TotalRoundsPre = -1) and (ClansCount = 1) then
+        TotalRoundsPre:= 0;
 
 // Determine clan ID to check to determine whether to increase TotalRoundsPre/TotalRoundsReal
 if (not PlacingHogs) then
