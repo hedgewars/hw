9b4e8df47713ee89a69b4ac5688844e8f9ec7fa8
unc0rr <unc0rr@gmail.com>
2018-11-24 23:54:32 +0100

    Format team.* code

--- qmlfrontend/team.cpp
+++ qmlfrontend/team.cpp
@@ -1,27 +1,20 @@
 #include "team.h"
 
 Hedgehog::Hedgehog()
-    : name(QObject::tr("unnamed", "default hedgehog name").toUtf8())
-    , hat("NoHat")
-    , hp(100)
-    , level(0)
-{
-}
+    : name(QObject::tr("unnamed", "default hedgehog name").toUtf8()),
+      hat("NoHat"),
+      hp(100),
+      level(0) {}
 
 Team::Team()
-    : name(QObject::tr("unnamed", "default team name").toUtf8())
-    , color("12345678")
-    , m_hedgehogsNumber(4)
-{
-    m_hedgehogs.resize(8);
+    : name(QObject::tr("unnamed", "default team name").toUtf8()),
+      color("12345678"),
+      m_hedgehogsNumber(4) {
+  m_hedgehogs.resize(8);
 }
 
-void Team::resize(int number)
-{
-    m_hedgehogsNumber = number;
-}
+void Team::resize(int number) { m_hedgehogsNumber = number; }
 
-QVector<Hedgehog> Team::hedgehogs() const
-{
-    return m_hedgehogs.mid(0, m_hedgehogsNumber);
+QVector<Hedgehog> Team::hedgehogs() const {
+  return m_hedgehogs.mid(0, m_hedgehogsNumber);
 }
--- qmlfrontend/team.h
+++ qmlfrontend/team.h
@@ -5,27 +5,27 @@
 #include <QVector>
 
 struct Hedgehog {
-    Hedgehog();
+  Hedgehog();
 
-    QByteArray name;
-    QByteArray hat;
-    quint32 hp;
-    int level;
+  QByteArray name;
+  QByteArray hat;
+  quint32 hp;
+  int level;
 };
 
 class Team {
-public:
-    explicit Team();
+ public:
+  explicit Team();
 
-    void resize(int number);
-    QVector<Hedgehog> hedgehogs() const;
+  void resize(int number);
+  QVector<Hedgehog> hedgehogs() const;
 
-    QByteArray name;
-    QByteArray color;
+  QByteArray name;
+  QByteArray color;
 
-private:
-    QVector<Hedgehog> m_hedgehogs;
-    int m_hedgehogsNumber;
+ private:
+  QVector<Hedgehog> m_hedgehogs;
+  int m_hedgehogsNumber;
 };
 
-#endif // TEAM_H
+#endif  // TEAM_H
