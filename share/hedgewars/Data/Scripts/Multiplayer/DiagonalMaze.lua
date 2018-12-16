9022b3e19ec3526db0e057eeb6c664834b6b5843
nemo <nemo@m8y.org>
2018-12-16 10:25:36 -0500

    fix DiagonalMaze after the drawing scaling change, add some params since ginormous maze is kinda entertaining

--- share/hedgewars/Data/Scripts/Multiplayer/DiagonalMaze.lua
+++ share/hedgewars/Data/Scripts/Multiplayer/DiagonalMaze.lua
@@ -1,13 +1,38 @@
+HedgewarsScriptLoad("/Scripts/Params.lua")
+local overrideFeatureSize = true
+local mazeScale = 0
 
 function onPreviewInit()
 onGameInit()
 end
 
+function onParameters()
+    parseParams()
+    if params["scalemap"] ~= nil then 
+        overrideFeatureSize = false 
+    end
+    if params["mazescale"] ~= nil then
+        mazeScale = tonumber(params["mazescale"])
+    end
+end
+
+
 function onGameInit()
+    local step
+    local width 
+
     MapGen = mgDrawn
     TemplateFilter = 0
-    local step = 80 + 10 * MapFeatureSize
-    local width = 1 + div(math.max(0, MapFeatureSize-12), 6)
+
+    if mazeScale > 0 then
+        step = 80 + 10 * mazeScale
+        width = 1 + div(math.max(0, mazeScale-12), 6)
+    else
+        step = 80 + 10 * MapFeatureSize
+        width = 1 + div(math.max(0, MapFeatureSize-12), 6)
+    end
+    -- reset feature size after use, to disable scaling
+    if overrideFeatureSize then MapFeatureSize = 12 end
     -- center maze
     local xoff = div((4000 % step), 2)
     for y = 48,2048,step do
