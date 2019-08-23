5027f9c4b98aafc7497aaba8bc9935101926ecbd
alfadur <none@none>
2019-08-23 19:26:52 +0300

    install win64 builds into ProgramFiles by default

--- tools/build_vcpkg.bat
+++ tools/build_vcpkg.bat
@@ -3,8 +3,11 @@ setlocal
 ::CONFIG START
 ::edit these variables if necessary
 
-::change between "Debug" and "Release"
-set BUILD_TYPE="Release"
+::change between Debug and Release
+set BUILD_TYPE=Release
+::path where Hedgewars will be installed to
+::default is %ProgramFiles%\hedgewars and requires running this script as administrator  
+set INSTALL_LOCATION=
 ::set if vcpkg is not on path
 set VCPKG_PATH=%VCPKG_ROOT%
 ::set if CMake is not on path
@@ -29,17 +32,30 @@ if "%VSCMD_ARG_TGT_ARCH%" == "x64" (
 )
 
 if "%FORCE_X64_CROSS_COMPILE%" NEQ "" (
-    set CROSS_COMPILE_FLAG="-DWIN32_WIN64_CROSS_COMPILE=1"
+    set CROSS_COMPILE_FLAG=-DWIN32_WIN64_CROSS_COMPILE=1
+    if "%INSTALL_LOCATION%" == "" (
+        set INSTALL_LOCATION=%ProgramFiles%/hedgewars
+    )
+) else (
+    set CROSS_COMPILE_FLAG=
+)
+
+if "%INSTALL_LOCATION%" NEQ "" (
+    set PREFIX_FLAG=-DCMAKE_INSTALL_PREFIX=%INSTALL_LOCATION%
+) else (
+    set PREFIX_FLAG=
 )
 
 if "%BUILD_SERVER%" == "" (
-    set BUILD_SERVER_FLAG="-DNOSERVER=1"
-)              
+    set BUILD_SERVER_FLAG=-DNOSERVER=1
+) else (
+    set BUILD_SERVER_FLAG=
+)             
 
 echo Running cmake...
 set ERRORLEVEL=
 
-cmake . -DCMAKE_TOOLCHAIN_FILE="%VCPKG_PATH%\scripts\buildsystems\vcpkg.cmake" -G"NMake Makefiles" "%CROSS_COMPILE_FLAG%" "%BUILD_SERVER_FLAG%" -DCMAKE_BUILD_TYPE="%BUILD_TYPE%" -DSDL2_BUILDING_LIBRARY=1
+cmake . -DCMAKE_TOOLCHAIN_FILE="%VCPKG_PATH%\scripts\buildsystems\vcpkg.cmake" -G"NMake Makefiles" %CROSS_COMPILE_FLAG% %BUILD_SERVER_FLAG% "%PREFIX_FLAG%" -DCMAKE_BUILD_TYPE="%BUILD_TYPE%" -DSDL2_BUILDING_LIBRARY=1
 
 if %ERRORLEVEL% NEQ 0 goto exitpoint
 
