310b14e7b7b5f201dfe57df40a9eac809a00d6b6
Wuzzy <Wuzzy2@mail.ru>
2020-06-29 13:17:10 +0200

    Increase X camera limit back to 2048
    Oops, the number 0 was done by mistake

--- hedgewars/uConsts.pas
+++ hedgewars/uConsts.pas
@@ -234,7 +234,7 @@ const
     cDefaultZoomLevel = 2.0; // 100% zoom
 
     // Maximum camera positions, values are "pixels outside the mainland"
-    cCamLimitX = 0; // X (left/right) camera limit, no border. Note: Influences sndFlyAway
+    cCamLimitX = 2048; // X (left/right) camera limit, no border. Note: Influences sndFlyAway
     cCamLimitY = 2048; // Top Y camera limit, no border
     cCamLimitBorderX = 2048; // X (left/right) camera limit, with border
     cCamLimitBorderY = 2048; // Top Y camera limit, with border
