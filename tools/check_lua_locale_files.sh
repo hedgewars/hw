f35801cc58c0fd733309339b5d28f07c1d1cd968
Wuzzy <Wuzzy2@mail.ru>
2020-12-07 16:59:44 +0100

    Lua locale check: Disable line length warning

--- tools/check_lua_locale_files.sh
+++ tools/check_lua_locale_files.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -
 echo "*** Luacheck of Lua locale files:"
-luacheck ../share/hedgewars/Data/Locale/*.lua --globals locale -q
+luacheck ../share/hedgewars/Data/Locale/*.lua --globals locale --no-max-line-length -q
 echo "Missing translations in Lua locale files:"
 grep -c -- "^--" ../share/hedgewars/Data/Locale/*.lua
