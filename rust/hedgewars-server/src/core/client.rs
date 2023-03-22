fa0fdd8a37bb39909c6f89ff5eaaebb549b70834
alfadur <none@none>
2023-03-22 18:53:00 +0300

    add a client moderator flag just to commit something

--- rust/hedgewars-server/src/core/client.rs
+++ rust/hedgewars-server/src/core/client.rs
@@ -10,6 +10,7 @@ bitflags! {
         const IS_CONTRIBUTOR = 0b0001_0000;
         const HAS_SUPER_POWER = 0b0010_0000;
         const IS_REGISTERED = 0b0100_0000;
+        const IS_MODERATOR = 0b1000_0000;
 
         const NONE = 0b0000_0000;
         const DEFAULT = Self::NONE.bits;
