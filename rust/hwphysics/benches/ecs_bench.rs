a220e0a1b2fb9564e85d63fc553e8e2145a18ebd
alfadur <none@none>
2023-03-28 17:53:30 +0300

    add iteration overhead benchmark

--- rust/hwphysics/benches/ecs_bench.rs
+++ rust/hwphysics/benches/ecs_bench.rs
@@ -52,6 +52,31 @@ pub fn component_run(c: &mut Criterion) {
     });
 }
 
+pub fn component_multirun(c: &mut Criterion) {
+    for n in (16..=64).step_by(16) {
+        let mut manager = GearDataManager::new();
+
+        manager.register::<P>();
+        manager.register::<V>();
+
+        for i in 1..=(SIZE / n) {
+            let gear_id = GearId::new(i as u16).unwrap();
+            manager.add(gear_id, &P::default());
+            manager.add(gear_id, &V::default());
+        }
+
+        c.bench_function(&format!("component run {}", n), |b| {
+            b.iter(|| {
+                for i in 0..n {
+                    manager
+                        .iter()
+                        .run(|(p, v): (&mut P, &mut V)| v.velocity += black_box(p.position));
+                }
+            })
+        });
+    }
+}
+
 pub fn component_add_remove(c: &mut Criterion) {
     let mut manager = GearDataManager::new();
     let mut gears1 = vec![];
@@ -84,5 +109,5 @@ pub fn component_add_remove(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, array_run, component_run, component_add_remove);
+criterion_group!(benches, array_run, component_run, component_multirun, component_add_remove);
 criterion_main!(benches);
