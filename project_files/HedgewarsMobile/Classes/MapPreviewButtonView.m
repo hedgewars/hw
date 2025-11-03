192456251887c9d6ed724facd0838bd93f506210
ciciplusplus <ciciplusplus@gmail.com>
2025-11-03 23:25:24 +0100

    Fix issue with crashing on a map preview generation
    --HG--
    branch : ios-develop

--- project_files/HedgewarsMobile/Classes/MapPreviewButtonView.m
+++ project_files/HedgewarsMobile/Classes/MapPreviewButtonView.m
@@ -86,10 +86,12 @@
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0), ^{
         NSString *ipcString = [[NSString alloc] initWithFormat:@"%ld", (long)self.port];
         NSString *documentsDirectory = DOCUMENTS_FOLDER();
+        NSString *resourcePath = [[NSString alloc] initWithFormat:@"%@/Data", [[NSBundle mainBundle] resourcePath]];
         
         NSMutableArray *gameParameters = [[NSMutableArray alloc] initWithObjects:
                                           @"--internal",
                                           @"--port", ipcString,
+                                          @"--prefix", resourcePath,
                                           @"--user-prefix", documentsDirectory,
                                           @"--landpreview",
                                           nil];
