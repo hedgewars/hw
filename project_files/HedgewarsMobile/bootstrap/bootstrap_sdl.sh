3b58f9d0a11001a1a1c0821bbbe4f4a8102ee336
ciciplusplus <ciciplusplus@gmail.com>
2025-11-03 23:19:03 +0100

    Add script to bootstrap SDL dependencies for the iOS build
    --HG--
    branch : ios-develop

---
+++ project_files/HedgewarsMobile/bootstrap/bootstrap_sdl.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# This script bootstraps SDL dependencies for the iOS build.
+# It also applies set of patches to fix "SDL.h not found" issue.
+#
+# Usage: cd project_files/HedgewarsMobile/bootstrap && ./bootstrap_sdl.sh
+
+set -ex
+
+mkdir ../../../../Library
+cd ../../../../Library
+# SDL
+git clone --branch release-2.32.10 --single-branch https://github.com/libsdl-org/SDL.git
+# SDL_image
+git clone --branch release-2.8.8 --single-branch https://github.com/libsdl-org/SDL_image.git && \
+cd SDL_image && git apply ../../hedgewars/project_files/HedgewarsMobile/bootstrap/SDL_image.patch && cd ..
+# SDL_image
+git clone --branch release-2.8.1 --single-branch https://github.com/libsdl-org/SDL_mixer.git && \
+cd SDL_mixer && git apply ../../hedgewars/project_files/HedgewarsMobile/bootstrap/SDL_mixer.patch && cd ..
+# SDL_net
+git clone --branch release-2.2.0 --single-branch https://github.com/libsdl-org/SDL_net.git && \
+cd SDL_net && git apply ../../hedgewars/project_files/HedgewarsMobile/bootstrap/SDL_net.patch && cd ..
+# SDL_ttf
+git clone --branch release-2.24.0 --single-branch https://github.com/libsdl-org/SDL_ttf.git && \
+cd SDL_ttf && git apply ../../hedgewars/project_files/HedgewarsMobile/bootstrap/SDL_ttf.patch && \
+cd external && ./download.sh
