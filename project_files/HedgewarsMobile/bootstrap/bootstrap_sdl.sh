0c229d82871def7261f7e5ac69554296e6924fe2
ciciplusplus <ciciplusplus@gmail.com>
2025-11-03 23:56:15 +0100

    Fix bootstrap script with correct patches
    --HG--
    branch : ios-develop

--- project_files/HedgewarsMobile/bootstrap/bootstrap_sdl.sh
+++ project_files/HedgewarsMobile/bootstrap/bootstrap_sdl.sh
@@ -13,14 +13,14 @@ cd ../../../../Library
 git clone --branch release-2.32.10 --single-branch https://github.com/libsdl-org/SDL.git
 # SDL_image
 git clone --branch release-2.8.8 --single-branch https://github.com/libsdl-org/SDL_image.git && \
-cd SDL_image && git apply ../../hedgewars/project_files/HedgewarsMobile/bootstrap/SDL_image.patch && cd ..
+cd SDL_image && git apply ../../hedgewars/project_files/HedgewarsMobile/bootstrap/SDL_image.diff && cd ..
 # SDL_image
 git clone --branch release-2.8.1 --single-branch https://github.com/libsdl-org/SDL_mixer.git && \
-cd SDL_mixer && git apply ../../hedgewars/project_files/HedgewarsMobile/bootstrap/SDL_mixer.patch && cd ..
+cd SDL_mixer && git apply ../../hedgewars/project_files/HedgewarsMobile/bootstrap/SDL_mixer.diff && cd ..
 # SDL_net
 git clone --branch release-2.2.0 --single-branch https://github.com/libsdl-org/SDL_net.git && \
-cd SDL_net && git apply ../../hedgewars/project_files/HedgewarsMobile/bootstrap/SDL_net.patch && cd ..
+cd SDL_net && git apply ../../hedgewars/project_files/HedgewarsMobile/bootstrap/SDL_net.diff && cd ..
 # SDL_ttf
 git clone --branch release-2.24.0 --single-branch https://github.com/libsdl-org/SDL_ttf.git && \
-cd SDL_ttf && git apply ../../hedgewars/project_files/HedgewarsMobile/bootstrap/SDL_ttf.patch && \
+cd SDL_ttf && git apply ../../hedgewars/project_files/HedgewarsMobile/bootstrap/SDL_ttf.diff && \
 cd external && ./download.sh
