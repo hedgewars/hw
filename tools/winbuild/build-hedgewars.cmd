cb639d8494a9eba30b961f89db6eb64485332c9b
unc0rr <unc0rr@gmail.com>
2026-01-07 13:57:52 +0100

    Fix a lot of installation paths on window, still WIP

--- tools/winbuild/build-hedgewars.cmd
+++ tools/winbuild/build-hedgewars.cmd
@@ -26,21 +26,43 @@ set OPENSSL_ROOT_DIR=%ROOTPATH%/openssl-master
 
 set CMAKE_PREFIX_PATH=%ROOTPATH%/ffmpeg;%ROOTPATH%/zlib1211;%ROOTPATH%/libpng16;%ROOTPATH%/physfs
 
-set GREP=Z:/w64devkit/bin/grep.exe
-set SED=Z:/w64devkit/bin/sed.exe
+set GREP=%ROOTPATH%/w64devkit/bin/grep.exe
+set SED=%ROOTPATH%/w64devkit/bin/sed.exe
 
 echo Removing old build dir...
 del /s /q "%BUILDDIR%"
-mkdir "%BUILDDIR%"
+%CMAKE% -E make_directory "%BUILDDIR%"
+
+set DLLS_DIR=%BUILDDIR%/win_dlls
+
+%CMAKE% -E make_directory "%DLLS_DIR%/plugins/tls" "%DLLS_DIR%/plugins/imageformats" "%DLLS_DIR%/plugins/platforms"
+
+copy /y "%SDL2_DIR%/bin/*.dll" "%DLLS_DIR%/"
+copy /y "%SDL2IMAGEDIR%/bin/*.dll" "%DLLS_DIR%/"
+copy /y "%SDL2NETDIR%/bin/*.dll" "%DLLS_DIR%/"
+copy /y "%SDL2TTFDIR%/bin/*.dll" "%DLLS_DIR%/"
+copy /y "%SDL2MIXERDIR%/bin/*.dll" "%DLLS_DIR%/"
+copy /y "%ROOTPATH%/ffmpeg/bin/*.dll" "%DLLS_DIR%/"
+copy /y "%ROOTPATH%/physfs/bin/*.dll" "%DLLS_DIR%/"
+copy /y "%Qt6_DIR%/bin/Qt6Core.dll" "%DLLS_DIR%/"
+copy /y "%Qt6_DIR%/bin/Qt6Gui.dll" "%DLLS_DIR%/"
+copy /y "%Qt6_DIR%/bin/Qt6Network.dll" "%DLLS_DIR%/"
+copy /y "%Qt6_DIR%/bin/Qt6Widgets.dll" "%DLLS_DIR%/"
+
+copy /y "%Qt6_DIR%/plugins/platforms/qwindows.dll" "%DLLS_DIR%/plugins/platforms"     
 
 echo Configuring...
 cd %BUILDDIR%
-%CMAKE% -G Ninja -DCMAKE_BUILD_TYPE="Release" -DWIN32_WIN64_CROSS_COMPILE=on -DCABAL_FLAGS="-j --project-file=tools/winbuild/cabal.project.local" %HWREPO%
+%CMAKE% -G Ninja -DCMAKE_BUILD_TYPE="Release" ^
+      -DWIN32_WIN64_CROSS_COMPILE=on ^
+      -DCABAL_FLAGS="-j --project-file=tools/winbuild/cabal.project.local" ^
+      -DEXTERNAL_BIN_DIR="%DLLS_DIR%" ^
+      %HWREPO%
 
 @if %errorlevel% neq 0 (
-    @echo Configure failed!
-    @exit /b 1
-@)
+    echo Configure failed!
+    exit /b 1
+)
 
 
 echo Building...
@@ -48,9 +70,9 @@ echo Building...
 %CMAKE% --build . --verbose --parallel
 
 @if %errorlevel% neq 0 (
-    @echo Build failed!
-    @exit /b 1
-@)
+    echo Build failed!
+    exit /b 1
+)
 
 echo Creating package...
 
